d6c713fa9f70b8366950cc1259276cbb4ca84d20
Luke Plant <L.Plant.98@cantab.net>
2012-01-24 13:57:27 +0000

    Added Bible version preference to Identity

---
+++ accounts/migrations/0002_auto__add_field_identity_default_bible_version.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Identity.default_bible_version'
+        db.add_column('accounts_identity', 'default_bible_version',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['bibleverses.BibleVersion'], null=True, blank=True),
+                      keep_default=False)
+
+    def backwards(self, orm):
+        # Deleting field 'Identity.default_bible_version'
+        db.delete_column('accounts_identity', 'default_bible_version_id')
+
+    models = {
+        'accounts.account': {
+            'Meta': {'object_name': 'Account'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 1, 24, 13, 32, 17, 349755)'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 1, 24, 13, 32, 17, 349579)'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'paid_until': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'subscription': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        'accounts.identity': {
+            'Meta': {'object_name': 'Identity'},
+            'account': ('django.db.models.fields.related.OneToOneField', [], {'default': 'None', 'to': "orm['accounts.Account']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 1, 24, 13, 32, 17, 348648)'}),
+            'default_bible_version': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bibleverses.BibleVersion']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'bibleverses.bibleversion': {
+            'Meta': {'object_name': 'BibleVersion'},
+            'full_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'slug': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'})
+        }
+    }
+
+    complete_apps = ['accounts']
