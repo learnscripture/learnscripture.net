643237f5c0b6264d1002991dfe7b6170c2c8b9dd
Luke Plant <L.Plant.98@cantab.net>
2012-01-24 14:23:16 +0000

    Added require_preferences, and fixed 'get_next'

--- learnscripture/decorators.py
+++ learnscripture/decorators.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.utils.functional import wraps
-
+from django.utils.http import urlquote
 
 from accounts.models import Identity
 
@@ -18,5 +20,16 @@ def require_identity(view_func):
         request.identity = identity
         if identity_id is None or identity.id != identity_id:
             request.session['identity_id'] = identity.id
+        return view_func(request, *args, **kwargs)
+    return view
+
+
+def require_preferences(view_func):
+    @wraps(view_func)
+    @require_identity
+    def view(request, *args, **kwargs):
+        identity = request.identity
+        if identity.default_bible_version is None:
+            return HttpResponseRedirect(reverse('preferences') + "?next=%s" % urlquote(request.get_full_path()))
         return view_func(request, *args, **kwargs)
     return view
--- learnscripture/views.py
+++ learnscripture/views.py
@@ -2,13 +2,14 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.utils.http import urlparse
 
 from accounts.forms import PreferencesForm
 
-from .decorators import require_identity
+from .decorators import require_identity, require_preferences
 
 
-@require_identity
+@require_preferences
 def learn(request):
     return render(request, 'learnscripture/learn.html')
 
@@ -20,17 +21,26 @@ def preferences(request):
         if form.is_valid():
             form.save()
             messages.info(request, "Prefences updated, thank you.")
-            return get_next(reverse('start'))
+            return get_next(request, reverse('start'))
     else:
         form = PreferencesForm(instance=request.identity)
     c = {'form':form}
     return render(request, 'learnscripture/preferences.html', c)
 
 
+def local_redirect(url):
+    """
+    Returns the URL if it is local, otherwise None
+    """
+    netloc = urlparse.urlparse(url)[1]
+    return None if netloc else url
+
+
 def get_next(request, default_url):
     if 'next' in request.GET:
-        next = request.GET['next']
-        return HttpResponseRedirect(next)
+        next = local_redirect(request.GET['next'])
+        if next is not None:
+            return HttpResponseRedirect(next)
 
     return HttpResponseRedirect(default_url)
 
