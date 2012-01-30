bb9826f9238517f26a75c2d22b847d3692a9d926
Luke Plant <L.Plant.98@cantab.net>
2012-01-30 13:23:31 +0000

    Added Account.username field

--- accounts/models.py
+++ accounts/models.py
@@ -20,6 +20,7 @@ SubscriptionType = make_choices('SubscriptionType',
 
 
 class Account(models.Model):
+    username = models.CharField(max_length=100, blank=False)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(unique=True)
