435def5c640c4744419b7b835909df87b1c08ff9
Luke Plant <L.Plant.98@cantab.net>
2012-02-07 03:43:18 +0000

    Switched to more reliable way of including font

--- learnscripture/static/js/theme.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-    // Avoid loading this unless we need it
-    var prefs = $('#id-preferences-data').data();
-    if (prefs.theme == 'bubblegum') {
-        $('head').append("<link href='http://fonts.googleapis.com/css?family=Short+Stack' rel='stylesheet' type='text/css'>");
-    }
-});
