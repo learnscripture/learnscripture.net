278591a56f018a8015798f8075559713e1a5500c
Luke Plant <L.Plant.98@cantab.net>
2012-02-13 20:16:29 +0000

    Re-enabled ?random on image URLs (the problem with this was probably only due to a setting Chrome developer tools)

--- learnscripture/settings.py
+++ learnscripture/settings.py
@@ -216,10 +216,6 @@ COMPRESS_PRECOMPILERS = (
     ('text/less', 'lessc {infile} {outfile}'),
 )
 
-# We don't want CssAbsoluteFilter because it adds ?randomdigits to URLs, and
-# some browsers (e.g. mine) decide not to cache the result.
-COMPRESS_CSS_FILTERS = []
-
 ### Sentry/Raven ###
 
 from settings_priv import SENTRY_DSN
