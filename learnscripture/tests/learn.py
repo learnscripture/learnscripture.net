320b2e8d012671e61d42a5f035cffcccfa1accb5
Luke Plant <L.Plant.98@cantab.net>
2012-02-27 01:09:00 +0000

    Fixed some fixture data

--- learnscripture/tests/learn.py
+++ learnscripture/tests/learn.py
@@ -94,7 +94,7 @@ class LearnTests(LiveServerTests):
         # This section can be replaced by a 'skip' button click once we've implemented that.
         for i in range(0, 9):
             driver.find_element_by_id('id-next-btn').click()
-        for word in "A psalm of David. The LORD is my shepherd, I lack nothing.".split():
+        for word in "The LORD is my shepherd, I lack nothing.".split():
             driver.find_element_by_id('id-typing').send_keys(word + ' ')
         driver.find_element_by_id('id-next-verse-btn').click()
 
