f15043d77b5e1db22a6751c0b1ff4eb2a56753e8
Luke Plant <L.Plant.98@cantab.net>
2012-02-29 02:09:16 +0000

    Correction to getPointsTarget

--- learnscripture/static/js/learn.js
+++ learnscripture/static/js/learn.js
@@ -588,8 +588,11 @@ var learnscripture =
             // Constants from scores/models.py
             var POINTS_PER_WORD = 10;
             var REVISION_BONUS_FACTOR = 2;
-            // Duplication of logic in Account.award_action_points
-            var points = wordList.length * POINTS_PER_WORD;
+            // Duplication of logic in Account.award_action_points.  NB word
+            // count excludes reference for this purpose, so we don't use
+            // wordList.count
+            var wordCount = stripPunctuation(currentVerseStatus.text.trim()).split(/\W/).length;
+            var points = wordCount * POINTS_PER_WORD;
             if (currentVerseStatus.memory_stage >= MEMORY_STAGE_TESTED) {
                 points = points * REVISION_BONUS_FACTOR;
             }
