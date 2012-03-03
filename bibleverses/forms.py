a3c2d5a93b357e6a1bddb10b02c2abdb29467ffc
Luke Plant <L.Plant.98@cantab.net>
2012-03-03 19:29:35 +0000

    Fixed bug with allowing whitespace for a VerseSet name

--- bibleverses/forms.py
+++ bibleverses/forms.py
@@ -98,6 +98,12 @@ class VerseSetForm(forms.ModelForm):
         model = VerseSet
         fields = ('name', 'description', 'public')
 
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        name = name.strip()
+        if name == '':
+            raise forms.ValidationError("This field is required")
+        return name
 
 a = VerseSetForm.base_fields['description'].widget.attrs
 del a['cols']
