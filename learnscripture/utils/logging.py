b4194c24bf392364547b1cae33b431e879b0de69
Luke Plant <L.Plant.98@cantab.net>
2012-03-15 17:17:48 +0000

    More robust logging.

--- learnscripture/utils/logging.py
+++ learnscripture/utils/logging.py
@@ -15,11 +15,20 @@ def sanitise_request_data(data):
         if http_data.get('method', '') == 'POST':
             post_data = http_data.get('data', None)
             if post_data is not None:
-                params = urlparse.parse_qs(post_data)
+                # Seems to be some confusion about whether post_data is a string
+                # or dict.
+                string_data = isinstance(post_data, basestring)
+                if string_data:
+                    params = urlparse.parse_qs(post_data)
+                else:
+                    params = post_data
                 for k in params.keys():
                     if SENSITIVE_RE.search(k):
                         params.pop(k)
-                http_data['data'] = urllib.urlencode(params.items(), doseq=True)
+                if string_data:
+                    http_data['data'] = urllib.urlencode(params.items(), doseq=True)
+                else:
+                    http_data['data'] = params
     return data
 
 def extra(**kwargs):
