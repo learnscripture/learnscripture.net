524318c24f889a79519c643ad9fb335d49bc6266
Luke Plant <L.Plant.98@cantab.net>
2012-03-16 17:17:08 +0000

    Added ability to search for verses by text

---
+++ bibleverses/fields.py
@@ -0,0 +1,16 @@
+from django.db import models
+from south.modelsinspector import add_introspection_rules
+
+add_introspection_rules([], ["^bibleverses\.fields\.VectorField"])
+
+
+class VectorField(models.Field):
+
+    def __init__( self, *args, **kwargs ):
+        kwargs['null'] = True
+        kwargs['editable'] = False
+        kwargs['serialize'] = False
+        super(VectorField, self).__init__(*args, **kwargs)
+
+    def db_type( self ):
+        return 'tsvector'
--- learnscripture/static/js/choose.js
+++ learnscripture/static/js/choose.js
@@ -137,6 +137,7 @@ var learnscripture =
                     success: loadResults,
                     error:  function(jqXHR, textStatus, errorThrown) {
                         if (jqXHR.status == 400) {
+                            $('#id_individual_search_results *').remove();
                             learnscripture.handleFormValidationErrors($('#id-verse-find-form'), '', jqXHR);
                         } else {
                             learnscripture.handlerAjaxError(jqXHR, textStatus, errorThrown);
@@ -148,8 +149,17 @@ var learnscripture =
         var loadResults = function(results) {
             $('#id-verse-find-form .validation-error').remove();
             var d = $('#id_individual_search_results');
-            d.html($('#id_search_result_template').render(results));
-            learnscripture.setupNeedsPreferencesControls(d);
+            if (results.length > 0) {
+                var html = '';
+                if (results.length > 10) {
+                    html = html + "<p>The first 10 results matching your search are below:</p>";
+                }
+                html = html + $('#id_search_result_template').render(results);
+                d.html(html);
+                learnscripture.setupNeedsPreferencesControls(d);
+            } else {
+                d.html("<p><span class='error'>No verses were found matching your search</span></p>");
+            }
         };
 
         var setupChooseControls = function() {
