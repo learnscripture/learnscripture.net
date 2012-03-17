104b2b9fb2394236ccf5b7805f82a9b858fc4d7b
Luke Plant <L.Plant.98@cantab.net>
2012-03-17 00:20:09 +0000

    Re-organised quickfind code to allow for re-use

--- learnscripture/static/js/choose.js
+++ learnscripture/static/js/choose.js
@@ -1,35 +1,15 @@
 var learnscripture =
     (function(learnscripture, $) {
-        var lookupVerse = function(ev) {
-            ev.preventDefault();
-            $.ajax({url: '/api/learnscripture/v1/versefind/',
-                    dataType: 'json',
-                    type: 'GET',
-                    data: {
-                        'quick_find': $(ev.target).closest('form').find('input[name=quick_find]').val(),
-                        'version_slug': $('#id-version-select').val()
-                    },
-                    success: loadResults,
-                    error:  function(jqXHR, textStatus, errorThrown) {
-                        if (jqXHR.status == 400) {
-                            $('#id_individual_search_results *').remove();
-                            learnscripture.handleFormValidationErrors($('#id-verse-find-form'), '', jqXHR);
-                        } else {
-                            learnscripture.handlerAjaxError(jqXHR, textStatus, errorThrown);
-                        }
-                    }
-                   });
-        };
 
         var loadResults = function(results) {
             $('#id-verse-find-form .validation-error').remove();
-            var d = $('#id_individual_search_results');
+            var d = $('.quickfind_search_results');
             if (results.length > 0) {
                 var html = '';
                 if (results.length > 10) {
                     html = html + "<p>The first 10 results matching your search are below:</p>";
                 }
-                html = html + $('#id_search_result_template').render(results);
+                html = html + $('#id_quickfind_result_template').render(results);
                 d.html(html);
                 learnscripture.setupNeedsPreferencesControls(d);
             } else {
@@ -38,7 +18,7 @@ var learnscripture =
         };
 
         var setupChooseControls = function() {
-            $('#id_lookup').click(lookupVerse);
+            $('#id_lookup').click(learnscripture.quickFindAndHandleResults(loadResults));
         };
 
         // Exports:
