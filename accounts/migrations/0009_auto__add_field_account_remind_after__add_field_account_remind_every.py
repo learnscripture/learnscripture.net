dfe59ec97a0d295f2ce91f82ac474d49842ac594
Luke Plant <L.Plant.98@cantab.net>
2012-03-21 23:59:00 +0000

    Added migration for Account.remind_after and Account.remind_every fields

---
+++ accounts/migrations/0009_auto__add_field_account_remind_after__add_field_account_remind_every.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Account.remind_after'
+        db.add_column('accounts_account', 'remind_after',
+                      self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=2),
+                      keep_default=False)
+
+        # Adding field 'Account.remind_every'
+        db.add_column('accounts_account', 'remind_every',
+                      self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=3),
+                      keep_default=False)
+
+    def backwards(self, orm):
+        # Deleting field 'Account.remind_after'
+        db.delete_column('accounts_account', 'remind_after')
+
+        # Deleting field 'Account.remind_every'
+        db.delete_column('accounts_account', 'remind_every')
+
+    models = {
+        'accounts.account': {
+            'Meta': {'object_name': 'Account'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 3, 21, 22, 48, 41, 19771)'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_tester': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 3, 21, 22, 48, 41, 19577)'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'paid_until': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'remind_after': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '2'}),
+            'remind_every': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
+            'subscription': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'accounts.identity': {
+            'Meta': {'object_name': 'Identity'},
+            'account': ('django.db.models.fields.related.OneToOneField', [], {'default': 'None', 'to': "orm['accounts.Account']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 3, 21, 22, 48, 41, 20823)'}),
+            'default_bible_version': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bibleverses.BibleVersion']", 'null': 'True', 'blank': 'True'}),
+            'enable_animations': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interface_theme': ('django.db.models.fields.CharField', [], {'default': "'calm'", 'max_length': '30'}),
+            'testing_method': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': 'None', 'null': 'True'})
+        },
+        'bibleverses.bibleversion': {
+            'Meta': {'ordering': "('short_name',)", 'object_name': 'BibleVersion'},
+            'full_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'slug': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'url': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '200', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['accounts']
