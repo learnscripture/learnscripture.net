41f9caac181c500ca223b2de349d229b44a45c8f
Luke Plant <L.Plant.98@cantab.net>
2012-03-28 22:31:08 +0100

    Fixed tests for subscribe page

--- learnscripture/tests/subscribe.py
+++ learnscripture/tests/subscribe.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
-from accounts.models import SubscriptionType
+from accounts.models import SubscriptionType, Account
 from .base import LiveServerTests
 
 SUBSCRIPTION_NOT_DUE = "Your subscription is not due for renewal"
@@ -23,9 +23,11 @@ class SubscribeTests(LiveServerTests):
     def test_no_payment_allowed_until_close_to_end_of_paid_period(self):
         identity, account = self.create_account()
 
-        account.subscription_type = SubscriptionType.PAID_UP
-        account.paid_until = timezone.now() + timedelta(100)
-        account.save()
+        Account.objects.filter(id=account.id).update(
+            subscription=SubscriptionType.PAID_UP,
+            # 100 > PAYMENT_ALLOWED_EARLY_DAYS
+            paid_until=timezone.now() + timedelta(100),
+            )
 
         self.login(account)
         driver = self.driver
@@ -34,11 +36,11 @@ class SubscribeTests(LiveServerTests):
 
     def test_payment_allowed_if_close_to_end_of_free_trial(self):
         identity, account = self.create_account()
-        self.login(account)
-
+        # (FREE_TRIAL_LENGTH_DAYS - PAYMENT_ALLOWED_EARLY_DAYS) < 50 < FREE_TRIAL_LENGTH_DAYS
         account.date_joined = timezone.now() - timedelta(50)
         account.save()
 
+        self.login(account)
         driver = self.driver
         driver.get(self.live_server_url + reverse('subscribe'))
         self.assertNotIn(SUBSCRIPTION_NOT_DUE, driver.page_source)
@@ -46,11 +48,22 @@ class SubscribeTests(LiveServerTests):
     def test_payment_allowed_if_close_to_end_of_paid_period(self):
         identity, account = self.create_account()
 
-        account.subscription = SubscriptionType.PAID_UP
-        account.paid_until = timezone.now() + timedelta(5)
-        account.save()
+        Account.objects.filter(id=account.id).update(
+            subscription=SubscriptionType.PAID_UP,
+            # 5 < PAYMENT_ALLOWED_EARLY_DAYS
+            paid_until=timezone.now() + timedelta(5),
+            )
 
         self.login(account)
         driver = self.driver
         driver.get(self.live_server_url + reverse('subscribe'))
         self.assertNotIn(SUBSCRIPTION_NOT_DUE, driver.page_source)
+
+    def test_no_payment_allowed_if_lifetime_free(self):
+        identity, account = self.create_account()
+        Account.objects.filter(id=account.id).update(subscription=SubscriptionType.LIFETIME_FREE)
+        self.login(account)
+        driver = self.driver
+        driver.get(self.live_server_url + reverse('subscribe'))
+        self.assertIn(SUBSCRIPTION_NOT_DUE, driver.page_source)
+
