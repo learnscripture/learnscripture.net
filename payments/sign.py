08e93a3a0cdca72d8d7951088d5791f25e3314d6
Luke Plant <L.Plant.98@cantab.net>
2012-04-10 21:51:40 +0100

    Switched to using django.core.signing for IPN 'custom' attribute
    This is necessary to support discounts.

---
+++ payments/sign.py
@@ -0,0 +1,22 @@
+import re
+
+from django.core.signing import loads, dumps, BadSignature
+
+
+def sign_payment_info(d):
+    """
+    Takes a dict of payment info, and returns a string with MAC
+    """
+    return dumps(d, salt='learnscripture.payments')
+
+
+def unsign_payment_string(s):
+    """
+    Takes a signed payment string, and returns a dict of payment
+    info if the signature is good, or None otherwise.
+    """
+    try:
+        # Only allow values 1 day old, that is more than enough time to complete payment
+        return loads(s, salt='learnscripture.payments', max_age=24*3600)
+    except BadSignature:
+        return None
