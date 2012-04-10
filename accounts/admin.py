1db694d02643877a7484569374a3731bcc72153b
Luke Plant <L.Plant.98@cantab.net>
2012-04-10 17:57:24 +0100

    Method for changing subscription type from the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 
-from .models import Identity, Account
+from .models import Identity, Account, SubscriptionType
+
+
+def change_subscription_func(subscription_type, name, description):
+    def change_subscription(modeladmin, request, queryset):
+        queryset.update(subscription=subscription_type)
+    change_subscription.short_description = description
+    change_subscription.__name__ = name
+    return change_subscription
+
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ['email', 'username', 'first_name', 'last_name',
                     'subscription', 'date_joined', 'paid_until',
                     'payment_possible']
     readonly_fields = ['subscription', 'paid_until']
+    actions = [
+        change_subscription_func(SubscriptionType.FREE_TRIAL,
+                                 "free_trial", "Change to free trial"),
+        change_subscription_func(SubscriptionType.BASIC,
+                                 "basic_account", "Change to basic account"),
+        change_subscription_func(SubscriptionType.LIFETIME_FREE,
+                                 "lifetime_free", "Change to lifetime free"),
+        ]
 
 admin.site.register(Identity)
 admin.site.register(Account, AccountAdmin)
