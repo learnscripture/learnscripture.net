e61f39eb98c6a9099f6612da082cc437f323e170
Luke Plant <L.Plant.98@cantab.net>
2012-04-16 18:06:10 +0100

    Fixed various issues with modals not being accessible
    1) If they were bigger than 600px tall
    2) If they changed size due to validation errors

--- learnscripture/static/js/modals.js
+++ learnscripture/static/js/modals.js
@@ -20,48 +20,61 @@ $(document).ready(function() {
               }
           }
       });
-    $('div.modal').bind('shown', function(ev) {
-        var modal = $(this);
+});
+
+
+var learnscripture = (function(learnscripture, $) {
+
+    var adjustModal = function(modal) {
         // if we have a small screen, then .modal divs are set to have
-        // 'position:absolute' instead of 'position:fixed' (using CSS media
-        // queries), so that they can scroll.  But this means the modal can be
-        // at the top of the screen, where it can't be seen if we are way down
-        // the screen.  We move the modal, being careful not to move it off
-        // bottom of screen.
-        if (modal.css('position') == 'absolute') {
-            var bestScrollTop;
-            var modalMargin = 10;  // allow 10px margin top and bottom, see CSS
-            var modalHeight = modal.height() + modalMargin * 2;
-            if ($(window).height() > modalHeight) {
-                // we have enough room for the whole modal
-                bestScrollTop = $(window).scrollTop();
-            } else {
-                // not enough room for modal, so be careful not to put it so
-                // that it would stick off the bottom of the page.
-                bestScrollTop = Math.min($(window).scrollTop(),
-                                         $(document).height() - modalHeight);
-            }
+        // 'position:absolute' instead of 'position:fixed', so that they can
+        // scroll.  But this means the modal can be at the top of the screen,
+        // where it can't be seen if we are way down the screen.  We move the
+        // modal, being careful not to move it off bottom of screen.
+
+        var bestScrollTop;
+        var modalMargin = 10;  // allow 10px margin top and bottom, see CSS
+        var modalHeight = modal.height() + modalMargin * 2;
+
+        if (modalHeight > $(window).height()) {
+            modal.css({'position': 'absolute',
+                       'top': modalMargin.toString() + 'px',
+                       'margin-top': '0px',
+                      });
+            // not enough room for modal, so be careful not to put it so
+            // that it would stick off the bottom of the page.
+            bestScrollTop = Math.min($(window).scrollTop(),
+                                     $(document).height() - modalHeight);
+
             if (modal.position().top != bestScrollTop) {
                 modal.css('top', (bestScrollTop + modalMargin).toString() + "px")
             }
+
+        } else {
+            modal.css({'position': 'fixed',
+                       'top': '50%',
+                       'margin-top': "-" + (modalHeight / 2).toString() + "px",
+                      });
+            // we have enough room for the whole modal
         }
 
+        modal.find('form').toggleClass('form-stacked', modal.width() < 500)
+    };
 
-        // For narrow screens, use 'form-stacked'
-        if (modal.width() < 500) {
-            modal.find('form').addClass('form-stacked');
-        }
 
-    }).bind('hidden', function(ev) {
+    $('div.modal').bind('shown', function(ev) {
         var modal = $(this);
-        // Remove any 'top' set above (for the case where browser window gets
-        // resized and we switch between absolute and fixed positioning for the
-        // modal).
-        modal.css('top', '');
-
-        // Remove the 'form-stacked' we added.
-        modal.find('form').removeClass('form-stacked');
+        adjustModal(modal);
     });
 
-});
+    $(window).bind('resize', function(ev) {
+        $('div.modal:visible').each(function(idx, elem) {
+            adjustModal($(elem));
+        })
+    });
 
+    // Export:
+    learnscripture.adjustModal = adjustModal;
+    return learnscripture;
+
+})(learnscripture || {}, $);
