db9f3f004e898bb16a76c1abc64b94481aedcf0b
Luke Plant <L.Plant.98@cantab.net>
2012-04-25 14:31:05 +0100

    Verse poups compatible with Android 1.5

--- learnscripture/static/js/versepopup.js
+++ learnscripture/static/js/versepopup.js
@@ -1,14 +1,15 @@
 var learnscripture = (function (learnscripture, $) {
     "use strict";
     var setupVersePopups = function () {
-        $('.verse-popup-ref').popover(
-            {
+        $('.verse-popup-ref')
+            .popover({
                 title: function () {
-                    return this.dataset.reference + " (" + this.dataset.version + ")";
+                    return this.attributes['data-reference'].value +
+                        " (" + this.attributes['data-version'].value + ")";
                 },
                 content: function () {
-                    var ref = this.dataset.reference;
-                    var version = this.dataset.version;
+                    var ref = this.attributes['data-reference'].value;
+                    var version = this.attributes['data-version'].value;
                     var content;
                     $.ajax({url: '/api/learnscripture/v1/versefind/',
                             dataType: 'json',
@@ -23,8 +24,14 @@ var learnscripture = (function (learnscripture, $) {
                             }
                            });
                     return content;
-                }
-            });
+                },
+                trigger: 'manual',
+            })
+            .bind('mouseenter', function(ev) { $(this).popover('show');})
+            .bind('mouseleave', function(ev) { $(this).popover('hide');})
+            .bind('focus', function(ev) { $(this).popover('show');})
+            .bind('blur', function(ev) { $(this).popover('hide');})
+        ;
     };
     // Exports
     learnscripture.setupVersePopups = setupVersePopups;
