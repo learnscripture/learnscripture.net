36329547f814299d734fed59e152952380057b79
Luke Plant <L.Plant.98@cantab.net>
2012-04-28 18:32:25 +0100

    Refactored out sending of awards notifications

---
+++ awards/hooks.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.dispatch import receiver
+
+from awards.signals import new_award
+
+
+@receiver(new_award)
+def notify_about_new_award(sender, **kwargs):
+    award = sender
+    account = award.account
+    account.identity.notices.create(message_html="""
+<img src="%s%s"> You've got a new award: <a href="%s">%s</a>""" %
+                                          (settings.STATIC_URL,
+                                           award.image_small(),
+                                           reverse('user_stats', args=(account.username,)),
+                                           award.short_description())
+                                          )
+
