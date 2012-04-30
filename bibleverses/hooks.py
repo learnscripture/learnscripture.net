7654a78a43ea4ee9d97904e68c5a39fcb382b687
Luke Plant <L.Plant.98@cantab.net>
2012-04-30 13:47:24 +0100

    Attempt to break import cycle

--- bibleverses/hooks.py
+++ bibleverses/hooks.py
@@ -1,6 +1,5 @@
 from django.dispatch import receiver
 
-from awards.tasks import give_verse_set_used_awards
 from bibleverses.tasks import verse_set_increase_popularity
 from bibleverses.signals import verse_set_chosen
 
@@ -8,5 +7,6 @@ from bibleverses.signals import verse_set_chosen
 @receiver(verse_set_chosen)
 def _verse_set_chosen(sender, **kwargs):
     verse_set = sender
+    from awards.tasks import give_verse_set_used_awards
     give_verse_set_used_awards.delay(verse_set.created_by_id)
     verse_set_increase_popularity.delay(verse_set.id)
