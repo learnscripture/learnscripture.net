d825dc722aacd98f088d31dcfcd591260ad18473
Luke Plant <L.Plant.98@cantab.net>
2012-05-07 19:01:44 +0100

    Pulled out a signal handler into the right module

--- bibleverses/hooks.py
+++ bibleverses/hooks.py
@@ -5,8 +5,6 @@ from bibleverses.signals import verse_set_chosen
 
 
 @receiver(verse_set_chosen)
-def _verse_set_chosen(sender, **kwargs):
+def verse_set_chosen_receiver(sender, **kwargs):
     verse_set = sender
-    from awards.tasks import give_verse_set_used_awards
-    give_verse_set_used_awards.delay(verse_set.created_by_id)
     verse_set_increase_popularity.delay(verse_set.id)
