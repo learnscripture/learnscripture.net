d4adbad1238e2d8586e83f87568e29c348d8a8d1
Luke Plant <L.Plant.98@cantab.net>
2012-05-07 17:20:39 +0100

    Added event stream to dashboard

--- events/models.py
+++ events/models.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+import math
+
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils import timezone
@@ -5,6 +8,19 @@ from django.utils.html import escape
 from jsonfield import JSONField
 
 from learnscripture.datastructures import make_choices
+
+
+EVENTSTREAM_CUTOFF_DAYS = 3 # just 3 days of events
+EVENTSTREAM_CUTOFF_NUMBER = 20
+
+# Arbitrarily say stuff is 50% less interesting when it is one day old.
+EVENTSTREAM_TIME_DECAY_FACTOR = 3600*24
+
+# This means that with EVENTSTREAM_CUTOFF_DAYS = 3, there is a factor of 8 to
+# play with: if an event has a weight more than 8 times higher than the rest of
+# the stream, it will stay at the top of the event stream until it disappears
+# altogether.
+
 
 EventType = make_choices('EventType',
                          [(1, 'NOTICE', 'Notice'),
@@ -46,7 +62,7 @@ class NoticeEvent(EventLogic):
     """
     Events used to broadcast notices to all users.
     """
-    weight = 1000
+    weight = 30
 
 
 class NewAccountEvent(EventLogic):
@@ -77,6 +93,17 @@ for event_type, c in EVENT_CLASSES.items():
     c.event_type = event_type
 
 
+class EventManager(models.Manager):
+    def for_dashboard(self, now=None):
+        if now is None:
+            now = timezone.now()
+
+        start = now - timedelta(EVENTSTREAM_CUTOFF_DAYS)
+        events = list(self.filter(created__gte=start))
+        events.sort(key=lambda e: e.get_rank(), reverse=True)
+        return events[0:EVENTSTREAM_CUTOFF_NUMBER]
+
+
 class Event(models.Model):
     message_html = models.TextField()
     event_type = models.PositiveSmallIntegerField(choices=EventType.choice_list)
@@ -84,5 +111,19 @@ class Event(models.Model):
     event_data = JSONField(blank=True)
     created = models.DateTimeField(default=timezone.now, db_index=True)
 
+    objects = EventManager()
+
     def __unicode__(self):
         return "Event %d" % self.id
+
+    def get_rank(self, now=None):
+        # In the future, we will want to include affinities in ranking, and will
+        # need UserEvent or something, but for now just use weight and recency.
+
+        if now is None:
+            now = timezone.now()
+
+        seconds = (now - self.created).total_seconds()
+
+        recency = 2 ** (-seconds/EVENTSTREAM_TIME_DECAY_FACTOR)
+        return self.weight * recency
