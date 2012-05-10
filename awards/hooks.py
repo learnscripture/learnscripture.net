5c0978f4647863c31993cd7d90a9f949b05f1bc6
Luke Plant <L.Plant.98@cantab.net>
2012-05-10 01:07:40 +0100

    More refactoring using signals

--- awards/hooks.py
+++ awards/hooks.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.dispatch import receiver
 
-from accounts.signals import new_account
+from accounts.signals import new_account, verse_tested
 from awards.signals import new_award, lost_award
 import awards.tasks
 from bibleverses.signals import verse_set_chosen
@@ -43,7 +43,6 @@ def notify_about_lost_award(sender, **kwargs):
     account.identity.notices.create(message_html=msg)
 
 
-
 @receiver(verse_set_chosen)
 def verse_set_chosen_receiver(sender, **kwargs):
     verse_set = sender
@@ -56,3 +55,11 @@ def new_account_receiver(sender, **kwargs):
     referrer_id = account.identity.referred_by_id
     if referrer_id is not None:
         awards.tasks.give_recruiter_award.apply_async([referrer_id], countdown=5)
+
+
+@receiver(verse_tested)
+def verse_tested_receiver(sender, **kwargs):
+    identity = sender
+    # Delay to allow this request's transaction to finish count to be updated.
+    awards.tasks.give_learning_awards.apply_async([identity.account_id],
+                                                  countdown=2)
