4750cd49087ddd04cdfa0d2124b05467f4a571ca
Luke Plant <L.Plant.98@cantab.net>
2012-05-15 10:08:56 +0100

    Better admin for Event

--- events/admin.py
+++ events/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import Event
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('id', 'event_type', 'weight', 'created')
+    list_display = ('id', 'account', 'event_type', 'weight', 'created')
+
+    def queryset(self, *args, **kwargs):
+        return super(EventAdmin, self).queryset(*args, **kwargs).select_related('account')
 
 admin.site.register(Event, EventAdmin)
