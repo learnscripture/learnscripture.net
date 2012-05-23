ce78f37769212d7002406a864e085b720cb20a10
Luke Plant <L.Plant.98@cantab.net>
2012-05-23 16:42:49 +0100

    Added models and admin for groups

---
+++ groups/models.py
@@ -0,0 +1,73 @@
+from autoslug import AutoSlugField
+from django.db import models
+from django.db.models import Count
+from django.utils import timezone
+
+from accounts.models import Account
+from learnscripture.datastructures import make_choices
+
+
+class GroupManager(models.Manager):
+    def with_membership_count(self):
+        return self.get_query_set().annotate(Count('members'))
+
+
+    def visible_for_account(self, account):
+        groups = Group.objects.with_membership_count()
+        public_groups = groups.filter(public=True)
+        visible_groups = public_groups
+
+        if account is not None:
+            account_groups = groups.filter(members=account)
+            visible_groups = visible_groups | account_groups
+
+            invited_groups = groups.filter(invitations__account=account)
+            visible_groups = visible_groups | invited_groups
+
+            created_groups = groups.filter(created_by=account)
+            visible_groups = visible_groups | created_groups
+
+        return visible_groups
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name', unique=True)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(Account, related_name='groups_created')
+    public = models.BooleanField()
+    open = models.BooleanField()
+    members = models.ManyToManyField(Account, through='Membership',
+                                     related_name='groups')
+
+
+    objects = GroupManager()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    account = models.ForeignKey(Account, related_name='memberships')
+    group = models.ForeignKey(Group, related_name='account_set')
+
+    created = models.DateTimeField(default=timezone.now)
+
+    def __unicode__(self):
+        return u'Membership of %s by %s' % (self.group, self.account)
+
+
+class InvitationManager(models.Manager):
+    def get_query_set(self):
+        return super(InvitationManager, self).get_query_set().select_related('account', 'group', 'created_by')
+
+class Invitation(models.Model):
+    account = models.ForeignKey(Account, related_name='invitations')
+    group = models.ForeignKey(Group, related_name='invitations')
+    created_by = models.ForeignKey(Account, related_name='invitations_created')
+
+    objects = InvitationManager()
+
+    def __unicode__(self):
+        return u'Invitation to %s for %s from %s' % (self.group, self.account, self.created_by)
