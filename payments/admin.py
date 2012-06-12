c7feb7f1f052fbc77c8f8db75324bad6b63335e7
Luke Plant <L.Plant.98@cantab.net>
2012-06-12 16:02:07 +0100

    Basic Fund model and admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Price, Currency, Payment
+from .models import Price, Currency, Payment, Fund
 
 
 class CurrencyAdmin(admin.ModelAdmin):
@@ -11,13 +11,23 @@ class PriceAdmin(admin.ModelAdmin):
     list_display = ['description', 'currency', 'amount', 'active']
 
 
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'account', 'amount', 'created']
+class FundAdmin(admin.ModelAdmin):
+    list_display = ['name', 'manager', 'balance']
+    filter_horizontal = ['members']
+    readonly_fields = ['balance']
 
     def queryset(self, request):
-        return super(PaymentAdmin, self).queryset(request).select_related('account')
+        return super(FundAdmin, self).queryset(request).select_related('manager')
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'account', 'fund', 'amount', 'created']
+
+    def queryset(self, request):
+        return super(PaymentAdmin, self).queryset(request).select_related('account', 'fund')
 
 
 admin.site.register(Currency, CurrencyAdmin)
 admin.site.register(Price, PriceAdmin)
+admin.site.register(Fund, FundAdmin)
 admin.site.register(Payment, PaymentAdmin)
