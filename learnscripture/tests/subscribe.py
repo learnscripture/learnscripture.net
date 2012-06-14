5fba416b84d7c0a8bf57245d447746747d2acdb4
Luke Plant <L.Plant.98@cantab.net>
2012-06-14 01:10:04 +0100

    Test for subscribe page when payment is overdue, and on free trial

--- learnscripture/tests/subscribe.py
+++ learnscripture/tests/subscribe.py
@@ -7,13 +7,18 @@ from django.test import TestCase
 from django.utils import timezone
 
 from accounts.models import SubscriptionType, Account
+from bibleverses.models import VerseSet
 from .base import LiveServerTests
 
 SUBSCRIPTION_NOT_DUE = "Your subscription is not due for renewal"
 
 class SubscribeTests(LiveServerTests):
 
-    fixtures = ['test_bible_versions.json', 'test_currencies.json', 'test_prices.json']
+    fixtures = ['test_bible_versions.json',
+                'test_bible_verses.json',
+                'test_verse_sets.json',
+                'test_currencies.json',
+                'test_prices.json']
 
     def test_no_payment_allowed_until_close_to_end_of_free_trial(self):
         identity, account = self.create_account()
@@ -46,6 +51,30 @@ class SubscribeTests(LiveServerTests):
         driver = self.driver
         driver.get(self.live_server_url + reverse('subscribe'))
         self.assertNotIn(SUBSCRIPTION_NOT_DUE, driver.page_source)
+
+    def test_payment_required_if_overdue(self):
+        identity, account = self.create_account()
+        # 100 > FREE_TRIAL_LENGTH_DAYS
+        account.date_joined = timezone.now() - timedelta(100)
+        account.save()
+        self.login(account)
+
+        # Add some verses.
+        vs1 = VerseSet.objects.get(name='Bible 101')
+        identity.add_verse_set(vs1)
+        identity.verse_statuses.filter(reference='John 3:16').update(
+            strength=0.7, last_tested=timezone.now())
+        identity.verse_statuses.filter(reference='John 14:6').update(
+            strength=0.75, last_tested=timezone.now())
+
+        driver = self.driver
+        # Check for redirect
+        driver.get(self.live_server_url + reverse('dashboard'))
+        self.wait_until_loaded('body')
+        self.assertTrue(driver.current_url.endswith(reverse('subscribe')))
+
+        # Check for text saying 'you've learned....'
+        self.assertIn(u'You could probably recite John 3:16 and John 14:6 right now', driver.page_source)
 
     def test_payment_allowed_if_close_to_end_of_paid_period(self):
         identity, account = self.create_account()
