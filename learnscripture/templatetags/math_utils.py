3bb7cb9527dc7b4475a64566ce9a6bd41d24a32f
Luke Plant <L.Plant.98@cantab.net>
2012-06-18 23:57:40 +0100

    Display info about fund discounts

---
+++ learnscripture/templatetags/math_utils.py
@@ -0,0 +1,14 @@
+from decimal import Decimal
+
+from django import template
+
+register = template.Library()
+from django.template.defaultfilters import floatformat
+
+@register.filter
+def percent(value):
+  if value is None:
+    return None
+  if isinstance(value, Decimal):
+      return str((value * Decimal('100')).quantize(Decimal('1'))) + '%'
+  return floatformat(value * 100.0, 2) + '%'
