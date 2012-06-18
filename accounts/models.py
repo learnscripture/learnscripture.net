10ad6eac7baa2fa3f1a4d775727234c8dede485c
Luke Plant <L.Plant.98@cantab.net>
2012-06-18 22:10:01 +0100

    Fixed a broken test
    Test broken under 1.4 due to different loading of one-to-one objects.
    
    New code should work under 1.4 and 1.5

--- accounts/models.py
+++ accounts/models.py
@@ -169,7 +169,7 @@ class Account(models.Model):
                                            accuracy=accuracy)
         # Change cached object to reflect DB, which has been
         # updated via a SQL UPDATE for max correctness.
-        self.total_score.points += points
+        self.total_score.points = current_points + points
         points_increase.send(sender=self,
                              previous_points=current_points,
                              points_added=score_log.points)
