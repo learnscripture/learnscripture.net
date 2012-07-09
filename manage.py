c0a02aeecd5678d439fee17c6a8825f6ec77457f
Luke Plant <L.Plant.98@cantab.net>
2012-07-09 17:34:05 +0100

    Louder about deprecation warnings

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os, sys
+import warnings
 
 if __name__ == "__main__":
+    warnings.filterwarnings("always", category=DeprecationWarning)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learnscripture.settings")
 
     from django.core.management import execute_from_command_line
