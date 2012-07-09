75b4a583e3625fab3a6b13c78fe1ace9b7bcd46f
Luke Plant <L.Plant.98@cantab.net>
2012-07-09 17:36:01 +0100

    Improvements to check_and_restore script

--- scripts/check_and_restore.py
+++ scripts/check_and_restore.py
@@ -8,20 +8,41 @@
 # tries to fix things.
 #
 # Logging is done in crontab. The script prints nothing if all is OK
-#
-# We use only stdlib imports
 
 from datetime import datetime
 from httplib import HTTPConnection
 import os
+import signal
 import socket
 import sys
 import time
+
+import psutil
 
 TIMEOUT = 20
 
 ATTEMPTS = 3
 SLEEP_BETWEEN_ATTEMPTS = 20
+USER = os.environ['USER']
+
+TARGETS = {
+    'development': {'APPNAME': 'learnscripture',
+                    'DOMAIN': 'learnscripture.local',
+                    'PORT': '8000',
+                    },
+    'staging': {'APPNAME': 'learnscripture_staging',
+                'DOMAIN': 'staging.learnscripture.net',
+                'PORT': '80',
+                },
+    'production': {'APPNAME': 'learnscripture',
+                   'DOMAIN':' learnscripture.net',
+                   'PORT': '80',
+                   },
+    }
+TARGET = None
+
+SUPERVISORCTL = "/home/cciw/webapps/learnscripture_django/venv/bin/supervisorctl"
+SUPERVISORD_STARTER = "/home/cciw/webapps/learnscripture_django/venv/bin/start_supervisor.sh"
 
 def print_message(msg):
     sys.stdout.write(datetime.now().isoformat() + "  " + str(msg) + "\n")
@@ -32,8 +53,7 @@ def site_is_up():
     # for specific known errors.
 
     try:
-        #connection = HTTPConnection('localhost', 8000, False, TIMEOUT)
-        connection = HTTPConnection('learnscripture.net', 80, False, TIMEOUT)
+        connection = HTTPConnection(TARGET['DOMAIN'], TARGET['PORT'], False, TIMEOUT)
         # We choose a URL that definitely needs the DB, to check the database is
         # working. We add a query param so that we can filter out in analytics,
         # and check this script is running
@@ -72,38 +92,83 @@ def check(attempts=ATTEMPTS):
     return False
 
 
-SUPERVISORCTL = "/home/cciw/webapps/learnscripture_django/venv/bin/supervisorctl"
-SUPERVISORD = "/home/cciw/webapps/learnscripture_django/venv/bin/supervisorctl"
-SUPERVISORD_STARTER = "/home/cciw/webapps/learnscripture_django/venv/bin/start_supervisor.sh"
-
-
 def start_supervisor():
     os.system("%s start" % SUPERVISORD_STARTER)
 
+def stop_supervisor():
+    os.system("%s stop" % SUPERVISORD_STARTER)
+
 
 def restore():
-    # First, try to start supervisor
+    # Sometimes supervisord itself can get in a 'bad state' of some kind, so we
+    # first shut it down, rather than assume it is in a good state.
+    try:
+        print_message("Stopping supervisord")
+        stop_supervisor()
+    except (Exception,), e:
+        print_message(str(e))
+
+    # Sometimes the problem is or could be a deadlocked cronjob, or some process
+    # that is triggering WebFaction's process killer due to high memory usage.
+    # Let's first kill everything that could be causing a problem.
+    try:
+        kill_cronjob_processes()
+    except (Exception,), e:
+        print_message(str(e))
+    # (This comes after stop_supervisor(), because some processes under
+    # supervisor's control could be misclassified as cronjob processes).
+
+    # Now restart
     try:
         print_message("Starting supervisord")
-        start_supervisord()
-    except:
-        pass
+        start_supervisor()
+    except (Exception,), e:
+        print_message(str(e))
 
-    time.sleep(10) # 10 seconds should be more than enough to get services up
-    if not check(attempts=1):
-        print_message("Site still not up")
-        # supervisord was probably running already.
-        # Try shutting down and restarting all services
-        print_message("Doing: supervisorctl restart all")
-        os.system("%s restart all" % SUPERVISORCTL)
+    print_message("Doing: supervisorctl start all")
+    os.system("%s start all" % SUPERVISORCTL)
 
-        # If still not up, the next time this script is called may fix things,
-        # otherwise we're out of options.
+    # There is also the apache process, which currently is not under the control
+    # of supervisord. It has its own cronjob to restart it, so we leave it
+    # alone.
 
+    # If still not up, the next time this script is called may fix things,
+    # otherwise we're out of options.
+
+
+def kill_cronjob_processes():
+    for ps in get_learnscripture_cronjob_processes():
+        print_message("Terminating process %d %s" % (ps.pid, ' '.join(ps.cmdline)))
+        ps.terminate()
+
+    # In case any didn't die
+    time.sleep(5)
+    for ps in get_learnscripture_cronjob_processes():
+        print_message("Killing process %d %s" % (ps.pid, ' '.join(ps.cmdline)))
+        ps.send_signal(signal.SIGKILL)
+
+def get_learnscripture_cronjob_processes():
+    # We only target ones that have 'manage.py' in cmdline. Importantly, that
+    # excludes this script!
+    return [ps for ps in get_controllable_processes()
+            if (TARGET['APPNAME'] in ps.getcwd() or TARGET['APPNAME'] in ' '.join(ps.cmdline))
+            and ('manage.py' in ' '.join(ps.cmdline))]
+
+def get_controllable_processes():
+    l = []
+    for ps in psutil.process_iter():
+        if ps.username == USER:
+            # Check we can get CWD, because we need that.
+            try:
+                cwd = ps.getcwd()
+            except psutil.AccessDenied:
+                continue
+            l.append(ps)
+    return l
 
 if __name__ == '__main__':
+    TARGET = TARGETS[sys.argv[1]]
+
     if not check():
         print_message("Attempting restore")
         restore()
-
-
