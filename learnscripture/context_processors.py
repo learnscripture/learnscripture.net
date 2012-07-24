cc9b8306f3f9e02d61acbe7eea8dcd647f50dab6
Luke Plant <L.Plant.98@cantab.net>
2012-07-24 23:36:55 +0100

    Made notices automatically expire and delete themselves 3 days after being seen

--- learnscripture/context_processors.py
+++ learnscripture/context_processors.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from fiber.context_processors import page_info
 
 from accounts.forms import PreferencesForm
@@ -8,6 +9,7 @@ from accounts.models import DEFAULT_THEME, THEME_FONTS
 from learnscripture.utils.context import lazy_dict
 from .forms import SignUpForm, LogInForm
 
+NOTICES_EXPIRE_AFTER_DAYS = 3
 
 def session_forms(request):
     # Use callables here to avoid overhead when not needed.  The template will
@@ -57,8 +59,22 @@ def menu(request):
 lazy_page_info = lazy_dict(page_info, ['fiber_page', 'fiber_current_pages'])
 
 def notices(request):
+    # Layer of laziness to avoid expiring notices unless actually rendered
+    def get_and_mark_notices():
+        l = []
+        for notice in request.identity.notices.all().order_by('created'):
+            if notice.seen is None:
+                notice.seen = timezone.now()
+                notice.save()
+            elif (timezone.now() - notice.seen).days > NOTICES_EXPIRE_AFTER_DAYS:
+                notice.delete()
+                notice = None
+            if notice is not None:
+                l.append(notice)
+        return l
+
     if hasattr(request, 'identity'):
-        return {'notices': request.identity.notices.all().order_by('created')}
+        return {'notices': get_and_mark_notices}
     else:
         return {}
 
