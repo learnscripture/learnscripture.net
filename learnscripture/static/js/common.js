e82d0a800d3a3f9e17e0f232557b48b663fa3897
Luke Plant <L.Plant.98@cantab.net>
2013-01-05 14:51:54 +0000

    Show notice if not on the latest Android app version

--- learnscripture/static/js/common.js
+++ learnscripture/static/js/common.js
@@ -143,7 +143,16 @@ var learnscripture = (function (learnscripture, $) {
 // Use [[ and ]] for templates to avoid clash with Django templates
 $.views.delimiters('[[', ']]');
 
-// Dropdown in topbar
 $(document).ready(function () {
+    // Dropdown in topbar
     $('.topbar').dropdown();
+
+    var CURRENT_ANDROID_APP_VERSION = 10;
+    if (window.androidlearnscripture &&
+        (window.androidlearnscripture.getVersionCode == undefined ||
+         window.androidlearnscripture.getVersionCode() < CURRENT_ANDROID_APP_VERSION)) {
+        $('.page-header').after(
+            '<div class="message-container"><div class="notice">Please <a href="https://play.google.com/store/apps/details?id=net.learnscripture.webviewapp">upgrade</a> to the most recent version of the Android app for the best experience.</div></div>'
+        );
+    }
 });
