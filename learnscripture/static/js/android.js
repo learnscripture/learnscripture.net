c3601eb9149ecbf955944b01831113c4fcdd6ca9
Luke Plant <L.Plant.98@cantab.net>
2013-01-05 15:44:05 +0000

    Mechanism to prompt users to rate the Android app after installation

--- learnscripture/static/js/android.js
+++ learnscripture/static/js/android.js
@@ -1,11 +1,31 @@
 $(document).ready(function () {
     "use strict";
     var CURRENT_ANDROID_APP_VERSION = 11;
-    if (window.androidlearnscripture &&
-        (window.androidlearnscripture.getVersionCode == undefined ||
-         window.androidlearnscripture.getVersionCode() < CURRENT_ANDROID_APP_VERSION)) {
-        $('.page-header').after(
-            '<div class="message-container"><div class="notice">Please <a href="https://play.google.com/store/apps/details?id=net.learnscripture.webviewapp">upgrade</a> to the most recent version of the Android app for the best experience.</div></div>'
-        );
+    if (window.androidlearnscripture) {
+        if (window.androidlearnscripture.getVersionCode == undefined ||
+            window.androidlearnscripture.getVersionCode() < CURRENT_ANDROID_APP_VERSION) {
+            $('.page-header').after(
+                '<div class="message-container"><div class="notice">Please <a href="https://play.google.com/store/apps/details?id=net.learnscripture.webviewapp">upgrade</a> to the most recent version of the Android app for the best experience.</div></div>'
+            );
+        }
+
+        var receiveAccountData = function (accountData) {
+            if (accountData && !accountData.hasInstalledAndroidApp) {
+                // According to recorded data, the user hasn't installed the Android
+                // app. But they have now, or we wouldn't have reached this code.
+                // So we update.
+                $.ajax({url: '/api/learnscripture/v1/androidappinstalled/?format=json',
+                        dataType: 'json',
+                        type: 'POST',
+                        data: {},
+                       });
+            }
+        };
+
+        receiveAccountData(learnscripture.getAccountData());
+        $('#id-account-data').bind('accountDataSet', function (ev, accountData) {
+            receiveAccountData(accountData);
+        });
+
     }
 });
