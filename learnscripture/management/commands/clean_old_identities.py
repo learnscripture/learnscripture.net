015089d8b6c812c5e6a2f0454100c77b5e841397
Luke Plant <L.Plant.98@cantab.net>
2013-01-15 22:17:11 +0000

    Added command for cleaning up unused Identity objects

---
+++ learnscripture/management/commands/clean_old_identities.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from django.core.management.base import BaseCommand
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            from accounts.models import Identity
+            from django.utils import timezone
+            from datetime import timedelta
+            from django.db.models import Count
+            from django.conf import settings
+
+            # For now, we keep old Identity objects that have some
+            # verse_statuses, because this might be useful for stats. Identity
+            # objects with nothing associated are definitely not interesting.
+            Identity.objects\
+                .filter(account__isnull=True,
+                        date_created__lt=timezone.now() - timedelta(days=settings.IDENTITY_EXPIRES_DAYS))\
+                        .annotate(verse_statuses_count=Count('verse_statuses'))\
+                        .filter(verse_statuses_count=0)\
+                        .delete()
+        except Exception as e:
+            logger.error("Couldn't clean old identities", exc_info=sys.exc_info())
