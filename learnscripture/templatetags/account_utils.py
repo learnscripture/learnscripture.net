6916cfd20fe5c23d6c15f93b15391e5d7be55e08
Luke Plant <L.Plant.98@cantab.net>
2013-01-21 15:29:02 +0000

    Create warning is link to inactive user is shown

--- learnscripture/templatetags/account_utils.py
+++ learnscripture/templatetags/account_utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.core.urlresolvers import reverse
 from django.template import Library
 
@@ -5,8 +7,13 @@ from learnscripture.utils.html import html_fragment
 
 register = Library()
 
+logger = logging.getLogger(__name__)
+
 @register.filter
 def account_link(account):
+    if not account.is_active:
+        logger.warning("Account link generated for inactive account %s", account,
+                       extra={'request', request})
     return html_fragment(u'<a href="%s" title="%s %s">%s</a>',
                          reverse('user_stats', args=(account.username,)),
                          account.first_name,
