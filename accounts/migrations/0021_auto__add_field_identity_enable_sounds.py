52709e97def05d2f1092becbd0f77570effc32b4
Luke Plant <L.Plant.98@cantab.net>
2013-04-18 17:54:16 +0100

    Added Identity.enable_sounds field

---
+++ accounts/migrations/0021_auto__add_field_identity_enable_sounds.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Identity.enable_sounds'
+        db.add_column(u'accounts_identity', 'enable_sounds',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Identity.enable_sounds'
+        db.delete_column(u'accounts_identity', 'enable_sounds')
+
+
+    models = {
+        u'accounts.account': {
+            'Meta': {'ordering': "['username']", 'object_name': 'Account'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'has_installed_android_app': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_hellbanned': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_tester': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_under_13': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'last_reminder_sent': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'remind_after': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '2'}),
+            'remind_every': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        u'accounts.identity': {
+            'Meta': {'object_name': 'Identity'},
+            'account': ('django.db.models.fields.related.OneToOneField', [], {'default': 'None', 'to': u"orm['accounts.Account']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'default_bible_version': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['bibleverses.TextVersion']", 'null': 'True', 'blank': 'True'}),
+            'enable_animations': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'enable_sounds': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interface_theme': ('django.db.models.fields.CharField', [], {'default': "'calm'", 'max_length': '30'}),
+            'referred_by': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'related_name': "'referrals'", 'null': 'True', 'blank': 'True', 'to': u"orm['accounts.Account']"}),
+            'testing_method': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': 'None', 'null': 'True'})
+        },
+        u'accounts.notice': {
+            'Meta': {'object_name': 'Notice'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'for_identity': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'notices'", 'to': u"orm['accounts.Identity']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message_html': ('django.db.models.fields.TextField', [], {}),
+            'seen': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'})
+        },
+        u'bibleverses.textversion': {
+            'Meta': {'ordering': "('short_name',)", 'object_name': 'TextVersion'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'slug': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'text_type': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'url': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '200', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['accounts']
