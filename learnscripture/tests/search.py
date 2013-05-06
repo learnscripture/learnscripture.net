050419cf5dcb8f6a3ac707b8d5264dc615b4ff18
Luke Plant <L.Plant.98@cantab.net>
2013-05-06 20:37:55 +0100

    Added ability to search verse sets by verse reference

--- learnscripture/tests/search.py
+++ learnscripture/tests/search.py
@@ -28,3 +28,24 @@ class SearchTests(TestCase):
                                           "Stupid")
         self.assertEqual(len(results), 1)
         self.assertIn("For stupid people", (v.name for v in results))
+
+    def test_search_verse_set_verses(self):
+        vs1 = VerseSet.objects.create(name="A selection",
+                                      slug="a-selection",
+                                      public=True,
+                                      set_type=VerseSetType.SELECTION,
+                                      created_by=self.account)
+        vs1.set_verse_choices([
+                "Genesis 1:1",
+                "Genesis 1:2",
+                "John 3:16",
+                ])
+
+        results = VerseSet.objects.search(VerseSet.objects.all(),
+                                          "Gen 1:3")
+        self.assertEqual(len(results), 0)
+
+        results = VerseSet.objects.search(VerseSet.objects.all(),
+                                          "Gen 1:1")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(list(results), [vs1])
