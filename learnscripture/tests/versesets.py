3f0ccd3f4941144cea555370019c234c8e9ae7fb
Luke Plant <L.Plant.98@cantab.net>
2013-05-14 10:25:12 +0100

    Tests for VerseSet visibility
    --HG--
    branch : feature_hellbanning

---
+++ learnscripture/tests/versesets.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+from django.test import TestCase
+
+from bibleverses.models import VerseSet, VerseSetType
+
+from .base import AccountTestMixin
+
+class VerseSetTests(AccountTestMixin, TestCase):
+
+    fixtures = ['test_bible_versions.json', 'test_bible_verses.json']
+
+    def test_visibility(self):
+        _, creator = self.create_account(username='creator')
+        _, viewer = self.create_account(username='viewer')
+
+        vs = VerseSet.objects.create(name="My set",
+                                     slug="my-set",
+                                     created_by=creator,
+                                     set_type=VerseSetType.SELECTION,
+                                     public=False)
+
+        # Private groups are visible to creator
+        self.assertIn(vs,
+                      VerseSet.objects.visible_for_account(creator)
+                      )
+
+        # But not to others.
+        self.assertNotIn(vs,
+                         VerseSet.objects.visible_for_account(viewer)
+                         )
+
+        vs.public = True
+        vs.save()
+
+        # public sets are visible
+        self.assertIn(vs,
+                      VerseSet.objects.visible_for_account(viewer)
+                      )
