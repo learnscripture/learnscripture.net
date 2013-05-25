f2047e8e06b208539d50f2eebc17dc34025c50ea
Luke Plant <L.Plant.98@cantab.net>
2013-05-25 17:38:34 +0100

    Show comment immediately when posting new comment
    --HG--
    branch : comment_system

--- comments/models.py
+++ comments/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.safestring import mark_safe
+from django.utils.html import urlize, linebreaks
 
 from accounts.models import Account
 from events.models import Event
 
+def format_comment_message(message):
+    return mark_safe(linebreaks(mark_safe(urlize(message.strip(), autoescape=True)), autoescape=False))
 
 class Comment(models.Model):
     author = models.ForeignKey(Account, related_name='comments')
     event = models.ForeignKey(Event, related_name='comments')
     created = models.DateTimeField(default=timezone.now)
     message = models.TextField()
+
+    @property
+    def message_formatted(self):
+        return format_comment_message(self.message)
