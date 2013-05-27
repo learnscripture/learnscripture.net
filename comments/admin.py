932dda2c5957a014a3246caad8fe9c3028a07c6c
Luke Plant <L.Plant.98@cantab.net>
2013-05-27 14:39:14 +0100

    Better admin for comments

--- comments/admin.py
+++ comments/admin.py
@@ -4,6 +4,7 @@ from .models import Comment
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['message', 'author', 'created', 'hidden']
+    raw_id_fields = ['event']
 
     def queryset(self, request):
         return super(CommentAdmin, self).queryset(request).select_related('author')
