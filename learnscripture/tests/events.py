0b3b67124c2f3e731dd46f46931bd75765e32b19
Luke Plant <L.Plant.98@cantab.net>
2013-05-27 13:21:30 +0100

    Added test for Event.objects.for_dashboard

--- learnscripture/tests/events.py
+++ learnscripture/tests/events.py
@@ -2,10 +2,12 @@ from __future__ import absolute_import
 
 from autofixture import AutoFixture
 from django.test import TestCase
+from django.utils import timezone
 
 from events.models import Event, EventType, NewCommentEvent
 from comments.models import Comment
 from accounts.models import Account
+from groups.models import Group
 
 from .base import AccountTestMixin
 
@@ -35,3 +37,44 @@ class EventTests(AccountTestMixin, TestCase):
         self.assertEqual(len([n for n in event_account.identity.notices.all()
                               if "You have new" in n.message_html]),
                          1)
+
+    def test_dashboard_stream(self):
+        _, account1 = self.create_account(username="1")
+        _, account2 = self.create_account(username="2")
+        _, viewer = self.create_account(username="viewer")
+        now = timezone.now()
+        e1 = Event.objects.create(
+            message_html="Event 1",
+            event_data={},
+            event_type=0,
+            created=now,
+            weight=10,
+            account=account1,
+            )
+        e2 = Event.objects.create(
+            message_html="Event 2",
+            event_data={},
+            event_type=0,
+            created=now,
+            weight=11,
+            account=account1,
+            )
+        e3 = Event.objects.create(
+            message_html="Event 3",
+            event_data={},
+            event_type=0,
+            created=now,
+            weight=11,
+            account=account2,
+            )
+
+        group = AutoFixture(Group).create(1)[0]
+        group.add_user(account2)
+        group.add_user(viewer)
+        stream = list(Event.objects.for_dashboard(account=viewer))
+
+        # e2 has greater weight than e1, should be earlier
+        self.assertTrue(stream.index(e2) < stream.index(e1))
+
+        # account2 and viewer are friends, so e3 should be before e2
+        self.assertTrue(stream.index(e3) < stream.index(e2))
