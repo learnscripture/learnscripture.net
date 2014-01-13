4fee3d7ac13ab6e06bc793ec9207926b475b03a3
Luke Plant <L.Plant.98@cantab.net>
2014-01-13 11:38:57 +0000

    Fixed circular import related to celery changes

--- bibleverses/tasks.py
+++ bibleverses/tasks.py
@@ -1,11 +1,11 @@
 from celery.task import task
 from django.db import models
 
-from bibleverses.models import VerseSet
 
 
 @task(ignore_result=True)
 def verse_set_increase_popularity(verse_set_id):
+    from bibleverses.models import VerseSet
     VerseSet.objects.filter(id=verse_set_id).update(popularity=models.F('popularity') + 1)
 
 
