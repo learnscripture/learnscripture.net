cce92be22fad377b87430184fbb1f5645fc8b54f
Luke Plant <L.Plant.98@cantab.net>
2014-07-23 12:12:25 +0100

    Debug tools

--- learnscripture/settings.py
+++ learnscripture/settings.py
@@ -245,7 +245,18 @@ ALLOWED_HOSTS = [".learnscripture.net"]
 
 if DEBUG:
     INSTALLED_APPS.append('debug_toolbar')
+    INSTALLED_APPS.append('devserver')
+    DEVSERVER_MODULES = (
+        #'devserver.modules.sql.SQLRealTimeModule',
+        'devserver.modules.sql.SQLSummaryModule',
+        #'devserver.modules.profile.ProfileSummaryModule',
 
+        # Modules not enabled by default
+        #'devserver.modules.ajax.AjaxDumpModule',
+        #'devserver.modules.profile.MemoryUseModule',
+        #'devserver.modules.cache.CacheSummaryModule',
+        #'devserver.modules.profile.LineProfilerModule',
+    )
 
 LOGGING = {
     'version': 1,
