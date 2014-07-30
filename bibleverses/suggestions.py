49625ca15f6e71d2aefaba5b8833d7b92504a881
Luke Plant <L.Plant.98@cantab.net>
2014-07-30 15:56:39 +0100

    Fixes/optimisations for Markov chain suggestions and thesaurus suggestions

--- bibleverses/suggestions.py
+++ bibleverses/suggestions.py
@@ -1,13 +1,21 @@
+# -*- coding: utf8 -*-
+
+from __future__ import unicode_literals
+
 from collections import Counter
+import glob
 import hashlib
+import pickle
 import random
 import re
+import sys
 
 import pykov
 
 from bibleverses.models import get_whole_book, TextType, BIBLE_BOOKS, WordSuggestionData, split_into_words, is_punctuation, Verse, QAPair
 
-PUNCTUATION = "!?,.<>()[];:\"'-"
+PUNCTUATION = "!?,.<>()[];:\"'-–"
+PUNCTUATION_OR_WHITESPACE = PUNCTUATION + " \n\r\t"
 IN_WORD_PUNCTUATION = "'-"
 
 # regexp for words with punctuation in the middle.
@@ -52,10 +60,13 @@ def split_into_words_for_suggestions(text):
     return map(normalise_word, split_into_words(text))
 
 
+def split_into_sentences(text):
+    return [s for s in [s.strip(PUNCTUATION_OR_WHITESPACE) for s in text.split('.')]
+            if s]
+
 def sentence_first_words(text):
     return [split_into_words_for_suggestions(l)[0]
-            for l in text.split('.')
-            if l]
+            for l in split_into_sentences(text)]
 
 
 TORAH = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy']
@@ -73,6 +84,7 @@ EPISTLES = ['Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians'
 groups = [TORAH, HISTORY, WISDOM, PROPHETS, NT_HISTORY, EPISTLES]
 
 MARKOV_CHAINS = {}
+
 
 def similar_books(book_name):
     retval = []
@@ -102,11 +114,11 @@ def generate_suggestions(version, ref=None, missing_only=True, thesaurus=None):
             items = get_whole_book(book, version).verses
             if is_done(items):
                 continue
-            training_text = " ".join(get_whole_book(b, version).text for b in similar_books(book))
+            training_texts = {(version.slug, b): get_whole_book(b, version).text for b in similar_books(book)}
             print book
             generate_suggestions_helper(version, items,
                                         lambda verse: verse.text,
-                                        training_text, ref=ref, missing_only=missing_only,
+                                        training_texts, ref=ref, missing_only=missing_only,
                                         thesaurus=thesaurus)
 
     elif version.text_type == TextType.CATECHISM:
@@ -117,21 +129,56 @@ def generate_suggestions(version, ref=None, missing_only=True, thesaurus=None):
         training_text = ' '.join(p.question + " " + p.answer for p in items)
         generate_suggestions_helper(version, items,
                                     lambda qapair: qapair.answer,
-                                    training_text,
+                                    {(version.slug, "all"): training_text},
                                     ref=ref, missing_only=missing_only,
                                     thesaurus=thesaurus)
 
+def sum_matrices(matrices):
+    # Do our own matrix sum to avoid n^2 behaviour
+    retval = pykov.Matrix()
+    keys = []
+    matrices = list(matrices)
+    for m in matrices:
+        keys.extend(m.keys())
+    keys = set(keys)
+    for k in keys:
+        retval[k] = sum(m[k] for m in matrices)
+    return retval
 
-def build_markov_chains_with_sentence_breaks(training_text, size):
+
+def build_markov_chains_with_sentence_breaks(training_texts, size):
+    return sum_matrices(build_markov_chains_for_text(key, training_text, size)
+                        for key, training_text in training_texts.items())
+
+def filename_for_labels(labels, size):
+    return "%s__level%s.markov.data" % ('_'.join(labels), str(size))
+
+
+def load_saved_markov_data(label_set):
+    keys = MARKOV_CHAINS.keys()
+    loaded_labels = set([k[0] for k in keys])
+    for labels in label_set:
+        if labels not in loaded_labels:
+            for fname in glob.glob(filename_for_labels(labels, '*')):
+                print "loading %s" % fname
+                MARKOV_CHAINS.update(pickle.load(file(fname)))
+
+
+def hash_text(text):
+    return hashlib.sha1(text.encode('utf-8')).hexdigest()
+
+
+def build_markov_chains_for_text(labels, text, size):
     # Look in dict first
-    hash = hashlib.sha1(training_text).hexdigest()
-    if (hash, size) in MARKOV_CHAINS:
-        return MARKOV_CHAINS[hash, size]
+    labels = tuple(labels)
+    if (labels, size) in MARKOV_CHAINS:
+        return MARKOV_CHAINS[labels, size]
 
-    sentences = training_text.split(".")
+    sentences = text.split(".")
     v_accum, c_accum = pykov.maximum_likelihood_probabilities([])
+    matrices = []
+    print "Markov analysis level %d for %s" % (size, labels)
     for i, s in enumerate(sentences):
-        print "Analysing sentence %d" % i
         if not s:
             continue
         words = split_into_words_for_suggestions(s)
@@ -140,78 +187,124 @@ def build_markov_chains_with_sentence_breaks(training_text, size):
         else:
             chain_input = [tuple(words[i:i+size]) for i in range(0, len(words)-(size-1))]
         v, c = pykov.maximum_likelihood_probabilities(chain_input, lag_time=1)
-        c_accum += c
+        matrices.append(c)
 
-    MARKOV_CHAINS[hash, size] = c_accum
-    return c_accum
+    retval = sum_matrices(matrices)
+    new_data = {(labels, size): retval}
+    MARKOV_CHAINS.update(new_data)
+    fname = filename_for_labels(labels, size)
+    with file(fname, "w") as f:
+        sys.stdout.write("Writing %s..." % fname)
+        pickle.dump(new_data, f)
+    sys.stdout.write("done\n")
+    return retval
 
 
-def generate_suggestions_helper(version, items, text_getter, training_text, ref=None,
+def generate_suggestions_helper(version, items, text_getter, training_texts, ref=None,
                                 missing_only=True, thesaurus=None):
-    first_words = sentence_first_words(training_text)
+    all_text = ' '.join(training_texts.values())
+    first_words = sentence_first_words(all_text)
     first_word_frequencies = frequency_pairs(first_words)
 
+    all_words = split_into_words_for_suggestions(all_text)
     markov_chains = {}
 
     def get_markov_chain(size):
         if size in markov_chains:
             return markov_chains[size]
         else:
-            c = build_markov_chains_with_sentence_breaks(training_text, size)
+            load_saved_markov_data(training_texts.keys())
+            c = build_markov_chains_with_sentence_breaks(training_texts, size)
             markov_chains[size] = c
             return c
 
-    def suggestions_thesaurus(words, i, count):
-        # Don't fill up with thesaurus answers, sometimes they are dumb,
-        # so limit to MIN_SUGGESTIONS
-        return [(w, 1.0) for w in thesaurus.get(words[i], [])][:MIN_SUGGESTIONS][:count]
+    def suggestions_thesaurus(words, i, count, suggestions_so_far):
+        # Thesaurus strategy can be dumb, due to words that are e.g. both nouns
+        # and verbs. Also, if suggestions are packed with synonyms, the basic
+        # meaning will be obvious.  So, we limit the number. Also, we spread
+        # them out 1.0 to 0.5 so there is a range.
+        # Note that thesaurus returns words already ordered
+        suggestions = thesaurus.get(words[i], [])[:MIN_SUGGESTIONS][:count]
+        if len(suggestions) == 0:
+            return []
+        inc = 0.5 / len(suggestions)
 
-    def suggestions_first_word(words, i, count):
+        return [(w, 1.0 - (n * inc)) for n, w in enumerate(suggestions)]
+
+    def suggestions_thesaurus_other(words, i, count, suggestions_so_far):
+        count = max(count, 10)
+        # This method tries to find some alternatives to words suggested by
+        # non-thesaurus methods, to throw people off the scent!
+        from_thesaurus = [w for w,f in suggestions_thesaurus(words, i, count, [])]
+        retval = []
+        new = set()
+        for w, f in suggestions_so_far:
+            if w not in from_thesaurus:
+                more = thesaurus.get(w, [])[:2] # Just two for each
+                for m in more:
+                    if m not in new:
+                        retval.append((m, f))
+                        new.add(m)
+            if len(retval) > count:
+                return retval
+        return retval
+
+    def suggestions_first_word(words, i, count, suggestions_so_far):
         return first_word_frequencies[:]
 
     def mk_suggestions_markov(size):
-        def suggestions_markov(words, i, count):
-            correct_word = words[i]
+        def suggestions_markov(words, i, count, suggestions_so_far):
             if size == 1:
                 start = words[i-1]
             else:
                 start = tuple(words[i-size:i])
             chain = get_markov_chain(size)
             options = chain.succ(start).items()
-            # We filter out the correct word, so that we know how many
-            # alternatives we have, and so probabilities for other
-            # words aren't skewed.
-            return [(w, f) for w, f in options if w != correct_word]
+            return [(w if size == 1 else w[-1], f) for w, f in options]
         return suggestions_markov
 
-    def suggestions_random(words, i, count):
-        retval = []
-        correct_word = words[i]
-        while len(retval) < count:
-            s = random.choice(words)
-            if s != correct_word:
-                retval.append((s, 1.0))
-        return retval
+    def suggestions_random_local(words, i, count, suggestions_so_far):
+        count = max(count, 10)
+        # Emphasise the words that come after the current one,
+        # exclude the one immediately before as that is very unlikely,
+        FACTOR = 5
+        bag = words[i+1:] * FACTOR + words[:max(0, i-1)]
+        if len(bag) == 0:
+            return []
+        c = Counter()
+        # Try to cope with the fact that we'll get duplicates by boosting the
+        # number we pick a bit.
+        for i in range(0, int(count * FACTOR / 2)):
+            c[random.choice(bag)] += 1
+        return c.items()
+
+    def suggestions_random_global(words, i, count, suggestions_so_far):
+        count = max(count, 10)
+        c = Counter()
+        for i in range(0, count):
+            c[random.choice(all_words)] += 1
+        return c.items()
 
     MIN_SUGGESTIONS = 30
-    MAX_SUGGESTIONS = 50
+    MAX_SUGGESTIONS = 60
 
     # Strategies for finding alternatives, ordered according to how good they will be
-    strategies = []
-    if thesaurus is not None:
-        strategies.append(
-            # Thesaurus isn't always very good, because
-            # it can come up with some dumb suggestions.
-            (lambda i: True, suggestions_thesaurus, 1)
-        )
 
-    strategies.extend([
-        (lambda i: i == 0, suggestions_first_word, 4),
-        (lambda i: i > 2, mk_suggestions_markov(3), 4),
-        (lambda i: i > 1, mk_suggestions_markov(2), 4),
-        (lambda i: i > 0, mk_suggestions_markov(1), 4),
-        (lambda i: True, suggestions_random, 4),
-    ])
+    strategies = [
+        (lambda i: i == 0, suggestions_first_word),
+        (lambda i: i > 2, mk_suggestions_markov(3)),
+        (lambda i: i > 1, mk_suggestions_markov(2)),
+        # Thesaurus isn't always very good, because it can give the game away,
+        # and also has some bizarre suggestions, so push down a bit
+        (lambda i: thesaurus is not None, suggestions_thesaurus),
+        # random from same verse are actually quite good,
+        # because there are lots of cases where you can
+        # miss out a word/phrase and it still makes sense
+        (lambda i: True, suggestions_random_local),
+        (lambda i: i > 0, mk_suggestions_markov(1)),
+        (lambda i: True, suggestions_random_global),
+        (lambda i: thesaurus is not None, suggestions_thesaurus_other),
+    ]
 
     to_create = []
     for item in items:
@@ -219,7 +312,6 @@ def generate_suggestions_helper(version, items, text_getter, training_text, ref=
             continue
 
         text = text_getter(item)
-        words = split_into_words_for_suggestions(text)
 
         # Clear out old suggestions
         existing = version.word_suggestion_data.filter(reference=item.reference)
@@ -232,39 +324,50 @@ def generate_suggestions_helper(version, items, text_getter, training_text, ref=
         print "%s %s" % (version.slug, item.reference)
 
         item_suggestions = []
-        for i, word in enumerate(words):
-            relevance = 1.0
-            word_suggestions = []
-            for condition, method, scale_factor in strategies:
-                if condition(i):
-                    need = MIN_SUGGESTIONS - len(word_suggestions) # Only last strategy really uses this
-                    new_suggestions = scale_suggestions(method(words, i, need), relevance)
-                    if len(new_suggestions) > 0:
-                        word_suggestions = merge_suggestions(word_suggestions, new_suggestions)
-                        relevance = relevance / scale_factor # scale down worse methods for finding suggestions
 
-            word_suggestions.sort(key=lambda (a,b): -b)
-            word_suggestions = scale_suggestions(word_suggestions[0:MAX_SUGGESTIONS])
+        # We are actually treating beginning of verse as beginning of sentence,
+        # which is not ideal, but hard to fix, and usually it's not too bad,
+        # because verse breaks are usually 'close' to being sentence breaks.
 
-            # Add hits=0
-            item_suggestions.append([(w,f,0) for w,f in word_suggestions])
+        sentences = split_into_sentences(text)
+        for sentence in sentences:
+            words = split_into_words_for_suggestions(sentence)
+            for i, word in enumerate(words):
+                relevance = 1.0
+                word_suggestions = []
+                for condition, method in strategies:
+                    if condition(i):
+                        need = MIN_SUGGESTIONS - len(word_suggestions) # Only random strategies really uses this
+                        new_suggestions = [(w,f) for (w,f) in method(words, i, need, word_suggestions)
+                                           if w != word]
+                        new_suggestions = scale_suggestions(new_suggestions, relevance)
+                        if len(new_suggestions) > 0:
+                            word_suggestions = merge_suggestions(word_suggestions, new_suggestions)
+                            relevance = relevance / 2.0 # scale down worse methods for finding suggestions
+
+                    # Sort after each one:
+                    word_suggestions.sort(key=lambda (a,b): -b)
+
+                word_suggestions = scale_suggestions(word_suggestions[0:MAX_SUGGESTIONS])
+
+                # Add hits=0
+                item_suggestions.append([(w,f,0) for w,f in word_suggestions])
 
         to_create.append(WordSuggestionData(version=version,
                                             reference=item.reference,
                                             suggestions=item_suggestions,
-                                            hash=hashlib.sha1(text),
+                                            hash=hash_text(text),
                                         ))
 
     WordSuggestionData.objects.bulk_create(to_create)
 
 
 def scale_suggestions(suggestions, factor=1.0):
-    # Scale frequencies to maxium of factor
+    # Scale frequencies to maximum of factor
     if len(suggestions) == 0:
         return suggestions
     max_f = max(f for w, f in suggestions)
     return [(w, float(f)/max_f * factor) for w, f in suggestions]
-
 
 def merge_suggestions(s1, s2):
     return (Counter(dict(s1)) + Counter(dict(s2))).items()
@@ -292,10 +395,10 @@ def get_all_suggestion_words(version_name):
 
 
 def get_all_version_words(version_name):
-    s = set()
+    text = []
     for verse in get_in_batches(Verse.objects.filter(version__slug=version_name)):
-        s |= set(split_into_words_for_suggestions(verse.text))
+        text.extend(split_into_words_for_suggestions(verse.text))
 
     for qapair in get_in_batches(QAPair.objects.filter(catechism__slug=version_name)):
-        s |= set(split_into_words_for_suggestions(qapair.answer))
-    return s
+        text.extend(split_into_words_for_suggestions(qapair.answer))
+    return Counter(text)
--- learnscripture/management/commands/setup_bibleverse_suggestions.py
+++ learnscripture/management/commands/setup_bibleverse_suggestions.py
@@ -1,31 +1,66 @@
+from __future__ import unicode_literals
 
+import pickle
 from django.core.management.base import BaseCommand
 
 import logging
 logger = logging.getLogger(__name__)
 
 def get_thesaurus(fname):
-    f = file(fname).read()
+    f = file(fname).read().decode('utf8')
     return dict((l.split(',')[0], l.split(',')[1:]) for l in f.split('\r'))
 
 
+
+# Thesaurus file tends to have unhelpful suggestions for pronouns, so we overwrite.
+# These are not synonyms, but likely alternatives
+PRONOUN_THESAURUS = {
+    'i': ['he', 'she','it','they', 'you', 'thou'],
+    'me': ['him', 'her', 'myself', 'them', 'you', 'thee'],
+    'he': ['she','it','i','they', 'you', 'thou'],
+    'him': ['her','it','me','them', 'you', 'thee', 'himself'],
+    'she': ['he','it','i','they', 'you', 'thou'],
+    'her': ['him','it','me','them', 'you', 'thee', 'herself'],
+    'it': ['she','he', 'him', 'her', 'i', 'me', 'they', 'them', 'you', 'thou', 'thee'],
+    'you': ['it', 'she','he', 'him', 'her', 'i', 'me', 'they', 'them', 'thou', 'thee'],
+    'we': ['it', 'she','he', 'i', 'they', 'thou'],
+    'us': ['him', 'her', 'me', 'them', 'you', 'thee'],
+}
+
+
 def version_thesaurus(version, base_thesaurus):
+    fname = version.slug + ".thesaurus"
+    try:
+        return pickle.load(file(fname))
+    except IOError:
+        pass
+
+    thesaurus = base_thesaurus.copy()
+    thesaurus.update(PRONOUN_THESAURUS)
+
     from bibleverses.suggestions import get_all_version_words
     d = {}
     print "Building thesaurus for %s" % version.slug
     words = get_all_version_words(version.slug)
-    for w in words:
-        alts = base_thesaurus.get(w, None)
+    for word, c in words.items():
+        alts = thesaurus.get(word, None)
         if alts is None:
-            d[w] = []
+            d[word] = []
             continue
 
         # Don't allow multi-word alternatives
         alts = [a for a in alts if not ' ' in a]
         # Don't allow alternatives that don't appear in the text
         alts = [a for a in alts if a in words]
-        # Normalise
-        d[w] = [a.lower() for a in alts]
+        # Normalise and exclude self
+        alts = [a.lower() for a in alts if a != word]
+        # Sort according to frequency in text
+        alts_with_freq = [(words[a], a) for a in alts]
+        alts_with_freq.sort(reverse=True)
+        d[word] = [w for c,w in alts_with_freq]
+
+    with file(fname, "w") as f:
+        pickle.dump(d, f)
     return d
 
 class Command(BaseCommand):
@@ -40,7 +75,7 @@ class Command(BaseCommand):
             thesaurus = None
             print "WARNING: Generating without thesaurus"
 
-        for v in TextVersion.objects.all()[0:1]:
+        for v in TextVersion.objects.all():
             print "=== " + v.slug + " ==="
             if thesaurus:
                 v_thesaurus = version_thesaurus(v, thesaurus)
