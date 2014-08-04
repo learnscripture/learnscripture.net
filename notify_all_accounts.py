5cdbbfc54216b0ab28d4bbee099e8c1c3cac7d71
Luke Plant <L.Plant.98@cantab.net>
2014-08-04 09:19:55 +0100

    Added script front end to send notification to all users.

---
+++ notify_all_accounts.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+
+
+if __name__ == '__main__':
+    message = sys.argv[1]
+
+    from fabfile import virtualenv, production, cd, run_venv, USER, HOST, env
+    env.host_string = "%s@%s" % (USER, HOST)
+
+    import pipes
+    production()
+    from fabfile import target
+    with virtualenv(target.VENV_DIR):
+        with cd(target.SRC_DIR):
+            run_venv("./manage.py send_notice_to_users " + pipes.quote(message))
