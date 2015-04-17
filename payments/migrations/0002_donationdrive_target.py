b7a701a9d3884713c17834411f8f88ba536b12ac
Luke Plant <L.Plant.98@cantab.net>
2015-04-17 08:50:35 +0100

    Add and display DonationDrive.target

---
+++ payments/migrations/0002_donationdrive_target.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from decimal import Decimal
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='donationdrive',
+            name='target',
+            field=models.DecimalField(default=Decimal('0'), max_digits=8, decimal_places=0),
+            preserve_default=True,
+        ),
+    ]
