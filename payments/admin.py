dee7c33dfe4cd704c154b98d2e4d88be47b6477f
Luke Plant <L.Plant.98@cantab.net>
2015-04-20 10:11:44 +0100

    Better admin for payments

--- payments/admin.py
+++ payments/admin.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 
 from .models import Payment, DonationDrive
 
+
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'account', 'amount', 'created']
+    def email(obj):
+        return obj.account.email
+
+    list_display = ['id', 'account', email, 'amount', 'created']
+    search_fields = ['account__username', 'account__email']
 
     def queryset(self, request):
         return super(PaymentAdmin, self).queryset(request).select_related('account')
 
 
+class DonationDriveAdmin(admin.ModelAdmin):
+    list_display = ['start', 'finish', 'active', 'hide_if_donated_days', 'target']
+    list_filter = ['active']
+
 admin.site.register(Payment, PaymentAdmin)
-admin.site.register(DonationDrive)
+admin.site.register(DonationDrive, DonationDriveAdmin)
