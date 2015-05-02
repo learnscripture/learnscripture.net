c534166c471cf00385f4eca36779e55a1a9a9441
Luke Plant <L.Plant.98@cantab.net>
2015-05-02 12:34:11 +0100

    Higher level utilities for writing Selenium tests

--- learnscripture/tests/test_account_details.py
+++ learnscripture/tests/test_account_details.py
@@ -13,17 +13,9 @@ class AccountDetailsTests(FullBrowserTest):
     def test_change_first_name(self):
         identity, account = self.create_account()
         self.login(account)
-        self.driver.get(self.live_server_url)
-        self.wait_until_loaded('body')
-
-        self.click("#id-session-menu")
-        # Should have an 'account' link
-
-        self.click('ul.dropdown-menu li a[href="/account/"]')
-
-        self.send_keys("#id_first_name", "Fred")
-        self.click("#id-save-btn")
-
+        self.get_url('account_details')
+        self.fill_input("#id_first_name", "Fred")
+        self.submit("#id-save-btn")
         self.assertEqual(Account.objects.get(id=account.id).first_name, "Fred")
 
     def test_redirect_if_not_logged_in(self):
