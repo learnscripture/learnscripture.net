7024a065a15cd12bc5183992a6688d73835dc6dc
Luke Plant <L.Plant.98@cantab.net>
2015-05-16 18:35:03 +0100

    Don't create word suggestion data when bulk loading verses

--- bibleverses/hooks.py
+++ bibleverses/hooks.py
@@ -8,6 +8,10 @@ from bibleverses.tasks import verse_set_increase_popularity, fix_item_suggestion
 from bibleverses.signals import verse_set_chosen
 
 
+def should_update_word_suggestions():
+    return not (settings.TESTING or getattr(settings, 'LOADING_VERSES', False))
+
+
 @receiver(verse_set_chosen)
 def verse_set_chosen_receiver(sender, **kwargs):
     verse_set = sender
@@ -17,12 +21,12 @@ def verse_set_chosen_receiver(sender, **kwargs):
 @receiver(post_save, sender=Verse)
 def verse_saved(sender, **kwargs):
     verse = kwargs['instance']
-    if not settings.TESTING:
+    if should_update_word_suggestions():
         fix_item_suggestions.delay(verse.version.slug, verse.reference)
 
 
 @receiver(post_save, sender=QAPair)
 def qapair_saved(sender, **kwargs):
     qapair = kwargs['instance']
-    if not settings.TESTING:
+    if should_update_word_suggestions():
         fix_item_suggestions.delay(qapair.catechism.slug, qapair.reference)
