eef719ebe10cc25f9a350eee7e66de9fea6583b6
Luke Plant <L.Plant.98@cantab.net>
2015-05-16 18:35:03 +0100

    Don't create word suggestion data when bulk loading verses

--- learnscripture/management/commands/load_catechism.py
+++ learnscripture/management/commands/load_catechism.py
@@ -1,5 +1,6 @@
 import json
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 import logging
@@ -11,11 +12,14 @@ class Command(BaseCommand):
 
     def handle(self, version_slug, json_filename, **options):
 
-        from bibleverses.models import TextVersion
+        from bibleverses.models import TextVersion, WordSuggestionData
         version = TextVersion.objects.get(slug=version_slug)
         json_data = json.load(file(json_filename))
 
+        settings.LOADING_VERSES = True
         version.qapairs.all().delete()
+        WordSuggestionData.objects.filter(version_slug=version.slug).delete()
+
         # Need to be able to handle things like question "2a"
         for item_num, (number, question, answer) in enumerate(json_data):
             version.qapairs.create(reference="Q{0}".format(number),
