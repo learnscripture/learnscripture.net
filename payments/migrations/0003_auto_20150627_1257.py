fc3a05e0a873e3b3b022d8ecf0ce1ae1832a9e43
Luke Plant <L.Plant.98@cantab.net>
2015-06-27 13:58:56 +0100

    noop migration

---
+++ payments/migrations/0003_auto_20150627_1257.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0002_donationdrive_target'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='payment',
+            name='paypal_ipn',
+            field=models.ForeignKey(related_name='payments', to='ipn.PayPalIPN'),
+            preserve_default=True,
+        ),
+    ]
