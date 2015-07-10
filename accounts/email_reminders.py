69a0033d61214f93d58ee6e1ba780f4bc4fbc23f
Luke Plant <L.Plant.98@cantab.net>
2015-07-10 15:04:57 +0100

    Handle rejected email addresses at SMTP level using mark_email_bounced.

--- accounts/email_reminders.py
+++ accounts/email_reminders.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from datetime import timedelta, datetime
 import email.parser
 import email.utils
+import smtplib
 
 from django.conf import settings
 from django.contrib.sites.models import get_current_site
@@ -23,43 +24,47 @@ def send_email_reminders():
     current_site = get_current_site(None)
     # remind_after == 0 mean 'never'
     for account in Account.objects.send_reminders_to().select_related('identity'):
-        # The whole loop could take some time, so we put this line inside loop:
-        n = timezone.now()
+        send_email_reminder(account, current_site)
 
-        # We will never send emails if the verse is overdue by less than
-        # 'remind_after' days, so use that to adjust our 'now' value and do DB
-        # filtering.
-        if account.identity is None:
-            # can occur in tests
-            continue
 
-        v = account.identity.first_overdue_verse(n + timedelta(account.remind_after))
-        if v is None:
-            continue
+def send_email_reminder(account, current_site):
+    n = timezone.now()
 
-        send_reminder = False
-        if (account.last_reminder_sent is not None and
-                account.last_reminder_sent > v.next_test_due):
-            # Reminder has been sent, so we are in the region
-            # for repeat reminders.
-            if (account.remind_every > 0 and  # remind_every == 0 'means' never
-                    (n - account.last_reminder_sent).days >= account.remind_every):
-                send_reminder = True
-        else:
-            # Reminder not sent, check for first reminder.
-            if (n - v.next_test_due).days >= account.remind_after:
-                send_reminder = True
+    # We will never send emails if the verse is overdue by less than
+    # 'remind_after' days, so use that to adjust our 'now' value and do DB
+    # filtering.
+    if account.identity is None:
+        # can occur in tests
+        return
 
-        if not send_reminder:
-            continue
+    v = account.identity.first_overdue_verse(n + timedelta(account.remind_after))
+    if v is None:
+        return
 
-        c = {'account': account,
-             'overdue_by': (n - v.next_test_due).days,
-             'domain': current_site.domain,
-             'site_name': current_site.name,
-             'login_token': get_login_token(account),
-             }
+    send_reminder = False
+    if (account.last_reminder_sent is not None and
+            account.last_reminder_sent > v.next_test_due):
+        # Reminder has been sent, so we are in the region
+        # for repeat reminders.
+        if (account.remind_every > 0 and  # remind_every == 0 'means' never
+                (n - account.last_reminder_sent).days >= account.remind_every):
+            send_reminder = True
+    else:
+        # Reminder not sent, check for first reminder.
+        if (n - v.next_test_due).days >= account.remind_after:
+            send_reminder = True
 
+    if not send_reminder:
+        return
+
+    c = {'account': account,
+         'overdue_by': (n - v.next_test_due).days,
+         'domain': current_site.domain,
+         'site_name': current_site.name,
+         'login_token': get_login_token(account),
+         }
+
+    try:
         EmailMessage(
             subject='Review reminder for LearnScripture.net',
             body=loader.render_to_string("learnscripture/reminder_email.txt", c),
@@ -68,6 +73,8 @@ def send_email_reminders():
             headers={'Auto-Submitted': 'auto-generated'},
         ).send()
         Account.objects.filter(id=account.id).update(last_reminder_sent=n)
+    except smtplib.SMTPRecipientsRefused:
+        mark_email_bounced(account.email, n)
 
 
 def handle_bounce(email_file):
