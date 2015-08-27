16488fa01fd19bbd7136782602e2a289ad0cda4d
Luke Plant <L.Plant.98@cantab.net>
2015-08-27 17:29:16 +0100

    Use opbeat in handle_reminder_bounces.py

--- scripts/handle_reminder_bounces.py
+++ scripts/handle_reminder_bounces.py
@@ -8,13 +8,16 @@ django.setup()
 
 
 def main(email_file):
+    import opbeat.contrib.django
     import raven.contrib.django.raven_compat
-    client = raven.contrib.django.raven_compat.DjangoClient()
+    sentry_client = raven.contrib.django.raven_compat.DjangoClient()
+    opbeat_client = opbeat.contrib.django.DjangoClient()
     from accounts.email_reminders import handle_bounce
     try:
         handle_bounce(email_file)
     except:
-        client.captureException()
+        sentry_client.captureException()
+        opbeat_client.capture_exception()
 
 if __name__ == '__main__':
     main(sys.stdin)
