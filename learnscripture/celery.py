43523b4c69498799fbd9cc9d89b1785ad99ab40a
Luke Plant <L.Plant.98@cantab.net>
2015-10-05 20:11:42 +0100

    Fully removed opbeat

--- learnscripture/celery.py
+++ learnscripture/celery.py
@@ -40,16 +40,3 @@ app = Celery('learnscripture')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-# Opbeat
-from opbeat.contrib.django.models import client, logger, register_handlers
-from opbeat.contrib.celery import register_signal
-
-try:
-    register_signal(client)
-except Exception as e:
-    logger.exception('Failed installing celery hook: %s' % e)
-
-if 'opbeat.contrib.django' in settings.INSTALLED_APPS:
-    register_handlers()
--- scripts/handle_reminder_bounces.py
+++ scripts/handle_reminder_bounces.py
@@ -8,16 +8,13 @@ django.setup()
 
 
 def main(email_file):
-    import opbeat.contrib.django
     import raven.contrib.django.raven_compat
     sentry_client = raven.contrib.django.raven_compat.DjangoClient()
-    opbeat_client = opbeat.contrib.django.DjangoClient()
     from accounts.email_reminders import handle_bounce
     try:
         handle_bounce(email_file)
     except:
         sentry_client.captureException()
-        opbeat_client.capture_exception()
 
 if __name__ == '__main__':
     main(sys.stdin)
