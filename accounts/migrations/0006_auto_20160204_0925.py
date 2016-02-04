eefc0b3bd5da1a29fef0dddf3a4bbd2e38c5a92d
Luke Plant <L.Plant.98@cantab.net>
2016-02-04 09:26:41 +0000

    Better admin for Identity.track_learning

---
+++ accounts/migrations/0006_auto_20160204_0925.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0005_auto_20151024_1859'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='identity',
+            name='track_learning',
+            field=models.BooleanField(default=False, help_text="Set this to enable detailed tracking of a user's learning, for debugging purposes."),
+        ),
+    ]
