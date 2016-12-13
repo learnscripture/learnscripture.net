d3c84181760450b7afd91dad058f50a6d03ea3df
Luke Plant <L.Plant.98@cantab.net>
2016-12-13 09:24:01 +0300

    Add Sentry exception logging to manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import logging
+import logging.config
 import os
 import sys
 import warnings
@@ -8,6 +10,15 @@ if __name__ == "__main__":
     warnings.filterwarnings("always", category=PendingDeprecationWarning)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learnscripture.settings")
 
+    from django.conf import settings  # NOQA isort:skip
+    from django.core import management  # NOQA isort:skip
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    logging.config.dictConfig(settings.LOGGING)
+    logger = logging.getLogger("management task exception")
+
+    try:
+        execute_from_command_line(sys.argv)
+    except Exception:
+        logger.exception(" ".join(sys.argv))
+        raise
