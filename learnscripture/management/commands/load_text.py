0d7f28ca3d5deb92497ced6730c140c0f7be6588
Luke Plant <L.Plant.98@cantab.net>
2017-06-06 17:27:46 +0300

    Use gzip for storing texts, for large savings.

--- learnscripture/management/commands/load_text.py
+++ learnscripture/management/commands/load_text.py
@@ -1,12 +1,15 @@
-from __future__ import unicode_literals, print_function
+from __future__ import print_function, unicode_literals
 
+import gzip
 import logging
 import os.path
 
-from django.db import transaction
 from django.conf import settings
 from django.core import serializers
 from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from .dump_text import BIBLE_ITEM_TEMPLATE, CATECHISM_ITEM_TEMPLATE, TEXTVERSION_TEMPLATE
 
 logger = logging.getLogger(__name__)
 
@@ -30,8 +33,8 @@ class Command(BaseCommand):
         for version_slug in options['version_slug']:
             print("Loading {0}".format(version_slug))
             with transaction.atomic():
-                text_file = "{0}.TextVersion.json".format(version_slug)
-                with file(os.path.join(input_dir, text_file), "rb") as f1:
+                text_file = TEXTVERSION_TEMPLATE.format(version_slug)
+                with gzip.open(os.path.join(input_dir, text_file), "rb") as f1:
                     objs = list(serializers.deserialize(format, f1, **common_options))
                     assert len(objs) == 1
                     obj = objs[0]
@@ -40,11 +43,11 @@ class Command(BaseCommand):
                     obj.save()
 
                 if version.text_type == TextType.CATECHISM:
-                    items_file = "{0}.QAPair.json".format(version_slug)
+                    items_file = CATECHISM_ITEM_TEMPLATE.format(version_slug)
                 elif version.text_type == TextType.BIBLE:
-                    items_file = "{0}.Verse.json".format(version_slug)
+                    items_file = BIBLE_ITEM_TEMPLATE.format(version_slug)
 
-                with file(os.path.join(input_dir, items_file), "rb") as f2:
+                with gzip.open(os.path.join(input_dir, items_file), "rb") as f2:
                     for i, obj in enumerate(serializers.deserialize(format, f2, **common_options)):
                         obj.save()
                         if (i + 1) % 100 == 0:
