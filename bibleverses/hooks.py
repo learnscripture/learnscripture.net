fc0b232f9e3fe5ebfc6682ca26a37057910d7c8e
Luke Plant <L.Plant.98@cantab.net>
2017-06-19 13:45:33 +0300

    Refactoring in Verse post_saved signals

--- bibleverses/hooks.py
+++ bibleverses/hooks.py
@@ -3,7 +3,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from bibleverses.models import QAPair, Verse
-from bibleverses.services import get_search_service
 from bibleverses.signals import verse_set_chosen
 from bibleverses.tasks import fix_item_suggestions, verse_set_increase_popularity
 
@@ -14,6 +13,11 @@ def should_update_word_suggestions_on_save():
                 getattr(settings, 'LOADING_WORD_SUGGESTIONS', False))
 
 
+def should_update_text_search_on_save():
+    # Happens to use the same criteria
+    return should_update_word_suggestions_on_save()
+
+
 @receiver(verse_set_chosen)
 def verse_set_chosen_receiver(sender, **kwargs):
     verse_set = sender
@@ -21,22 +25,32 @@ def verse_set_chosen_receiver(sender, **kwargs):
 
 
 @receiver(post_save, sender=Verse)
-def verse_saved(sender, **kwargs):
+def verse_saved_update_word_suggestions(sender, **kwargs):
+    from bibleverses.suggestions import item_suggestions_need_updating
     verse = kwargs['instance']
-    if should_update_word_suggestions_on_save():
-        from bibleverses.suggestions import item_suggestions_need_updating
-        # We definitely have text now, might not later,
-        # so do this check at this point to avoid unnecessary
-        # work and refetching the data from the API service
-        if not item_suggestions_need_updating(verse):
-            return
-        fix_item_suggestions.apply_async([verse.version.slug, verse.reference],
-                                         countdown=5)
+    if not should_update_word_suggestions_on_save():
+        return
 
-        if get_search_service(verse.version.slug) is None:
-            # No external search service, therefore must be using builtin DB
-            # search, therefore need to update:
-            Verse.objects.filter(id=verse.id).update_text_search()
+    # We definitely have text now, might not later, so do this check at this
+    # point to avoid unnecessary work and refetching the data from the API
+    # service
+    if not item_suggestions_need_updating(verse):
+        return
+
+    fix_item_suggestions.apply_async([verse.version.slug, verse.reference],
+                                     countdown=5)
+
+
+@receiver(post_save, sender=Verse)
+def verse_saved_update_text_search(sender, **kwargs):
+    verse = kwargs['instance']
+    if not should_update_text_search_on_save():
+        return
+
+    if verse.version.db_based_searching:
+        # No external search service, therefore must be using builtin DB
+        # search, therefore need to update:
+        Verse.objects.filter(id=verse.id).update_text_search()
 
 
 @receiver(post_save, sender=QAPair)
