25b8a6e516385e21676f6866f2dfa39a28eca8c4
Luke Plant <L.Plant.98@cantab.net>
2017-08-27 22:44:09 +0300

    Large memory win for Markov chains

---
+++ bibleverses/suggestions/utils/memory.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def intern_it(item):
+    if isinstance(item, (tuple, list)):
+        return item.__class__(intern_it(i) for i in item)
+    elif isinstance(item, str):
+        return sys.intern(item)
+    else:
+        return item
