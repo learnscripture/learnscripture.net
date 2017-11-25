b711602eaac0c6947ded1e3b10f6aeefdfe343be
Luke Plant <L.Plant.98@cantab.net>
2017-11-25 12:05:49 +0300

    Separate CSS files instead of inline

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,13 @@
 var BundleTracker = require('webpack-bundle-tracker')
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var config = require('./webpack.config.base.js');
 
 config.output.filename = "[name]-[hash].dev.js";
 
 config.plugins = config.plugins.concat([
-    new BundleTracker({filename: './webpack-stats.dev.json'})
+    new BundleTracker({filename: './webpack-stats.dev.json'}),
+    new ExtractTextPlugin("[name]-[hash].dev.css"),
 ])
 
 module.exports = config;
