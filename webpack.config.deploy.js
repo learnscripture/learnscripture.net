134ab809eb771a6c37482571e39c44eb2f0a90f5
Luke Plant <L.Plant.98@cantab.net>
2017-11-27 21:48:11 +0300

    Added webpack deployment code

---
+++ webpack.config.deploy.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var config = require('./webpack.config.base.js');
+
+config.output.filename = "[name]-[hash].deploy.pack.js";
+
+config.plugins = config.plugins.concat([
+    new BundleTracker({filename: './webpack-stats.deploy.json'}),
+    new ExtractTextPlugin("[name]-[hash].deploy.css"),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true
+    })
+])
+
+module.exports = config;
