b9a9d6627f713c6bc4f4b0572918729cebeaf1ca
Luke Plant <L.Plant.98@cantab.net>
2017-11-30 15:53:05 +0300

    Pass account data to Elm learn page

--- learnscripture/static/elm/Learn.elm
+++ learnscripture/static/elm/Learn.elm
@@ -5,8 +5,10 @@ import Html exposing (div, button, text, programWithFlags)
 
 {- Flags and external inputs -}
 
+
 type alias Flags =
     { preferences : JsPreferences
+    , account : Maybe AccountData
     }
 
 
@@ -18,6 +20,7 @@ type alias JsPreferences =
     , desktopTestingMethod : String
     , touchscreenTestingMethod : String
     }
+
 
 testingMethodFromString : String -> TestingMethod
 testingMethodFromString x =
@@ -37,21 +40,42 @@ testingMethodFromString x =
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    let p = flags.preferences
-    in ( { preferences = { preferencesSetup = p.preferencesSetup
-                         , enableAnimations = p.enableAnimations
-                         , enableSounds = p.enableSounds
-                         , enableVibration = p.enableVibration
-                         , desktopTestingMethod = testingMethodFromString p.desktopTestingMethod
-                         , touchscreenTestingMethod = testingMethodFromString p.touchscreenTestingMethod
-                         }
-         }, Cmd.none)
+    let
+        p =
+            flags.preferences
+
+        a =
+            flags.account
+    in
+        ( { preferences =
+                { preferencesSetup = p.preferencesSetup
+                , enableAnimations = p.enableAnimations
+                , enableSounds = p.enableSounds
+                , enableVibration = p.enableVibration
+                , desktopTestingMethod = testingMethodFromString p.desktopTestingMethod
+                , touchscreenTestingMethod = testingMethodFromString p.touchscreenTestingMethod
+                }
+          , user =
+                case a of
+                    Just ad ->
+                        Account ad
+
+                    Nothing ->
+                        GuestUser
+          }
+        , Cmd.none
+        )
+
+
 
 {- Model -}
 
+
 type alias Model =
     { preferences : Preferences
+    , user : User
     }
+
 
 type alias Preferences =
     { preferencesSetup : Bool
@@ -62,11 +86,21 @@ type alias Preferences =
     , touchscreenTestingMethod : TestingMethod
     }
 
+
 type TestingMethod
     = FullWords
     | FirstLetter
     | OnScreen
 
+
+type User
+    = GuestUser
+    | Account AccountData
+
+
+type alias AccountData =
+    { username : String
+    }
 
 
 main : Program Flags Model Msg
@@ -82,12 +116,22 @@ main =
 view : Model -> Html.Html msg
 view model =
     div []
-        [ div [] [ text "Hello world! How are you?" ]
+        [ div [] [ text ("Hello " ++ userDisplayName model.user ++ "! How are you?") ]
         , div [] [ text ("preferencesSetup: " ++ toString model.preferences.preferencesSetup) ]
         , div [] [ text ("enableVibration: " ++ toString model.preferences.enableVibration) ]
         , div [] [ text ("enableAnimations: " ++ toString model.preferences.enableAnimations) ]
         , div [] [ text ("desktopTestingMethod: " ++ toString model.preferences.desktopTestingMethod) ]
         ]
+
+
+userDisplayName : User -> String
+userDisplayName u =
+    case u of
+        GuestUser ->
+            "Guest"
+
+        Account ad ->
+            ad.username
 
 
 subscriptions : a -> Sub msg
