5b74b624e19200840e7dbb90135c92c2cb016573
Luke Plant <L.Plant.98@cantab.net>
2017-11-30 15:30:57 +0300

    Pass preferences data to elm

--- learnscripture/static/js/learn_setup.js
+++ learnscripture/static/js/learn_setup.js
@@ -7,4 +7,18 @@
 import 'learn.less';
 
 import Elm from "../elm/Learn";
-Elm.Main.embed(document.getElementById('elm-main'));
+var preferencesNode = document.getElementById('id-preferences-data');
+
+var preferences =
+    Elm.Main.embed(
+        document.getElementById('elm-main'),
+        {
+            "preferences": {
+                "preferencesSetup": preferencesNode.attributes['data-preferences-setup'].value == "true",
+                "enableAnimations": preferencesNode.attributes['data-enable-animations'].value == "true",
+                "enableSounds": preferencesNode.attributes['data-enable-sounds'].value == "true",
+                "enableVibration": preferencesNode.attributes['data-enable-vibration'].value == "true",
+                "desktopTestingMethod": preferencesNode.attributes['data-desktop-testing-method'].value,
+                "touchscreenTestingMethod": preferencesNode.attributes['data-touchscreen-testing-method'].value,
+            }
+        });
