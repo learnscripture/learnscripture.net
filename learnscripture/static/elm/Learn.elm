e284d656d214f56a575ef6732410f712dbee19b5
Luke Plant <L.Plant.98@cantab.net>
2017-11-30 15:30:57 +0300

    Pass preferences data to elm

--- learnscripture/static/elm/Learn.elm
+++ learnscripture/static/elm/Learn.elm
@@ -1,19 +1,106 @@
+module Main exposing (..)
 
-import Html exposing (beginnerProgram, div, button, text)
-import Html.Events exposing (onClick)
+import Html exposing (div, button, text, programWithFlags)
 
 
+{- Flags and external inputs -}
+
+type alias Flags =
+    { preferences : JsPreferences
+    }
+
+
+type alias JsPreferences =
+    { preferencesSetup : Bool
+    , enableAnimations : Bool
+    , enableSounds : Bool
+    , enableVibration : Bool
+    , desktopTestingMethod : String
+    , touchscreenTestingMethod : String
+    }
+
+testingMethodFromString : String -> TestingMethod
+testingMethodFromString x =
+    case x of
+        "FULL_WORDS" ->
+            FullWords
+
+        "FIRST_LETTER" ->
+            FirstLetter
+
+        "ON_SCREEN" ->
+            OnScreen
+
+        x ->
+            Debug.crash ("Uknown testing method " ++ x)
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    let p = flags.preferences
+    in ( { preferences = { preferencesSetup = p.preferencesSetup
+                         , enableAnimations = p.enableAnimations
+                         , enableSounds = p.enableSounds
+                         , enableVibration = p.enableVibration
+                         , desktopTestingMethod = testingMethodFromString p.desktopTestingMethod
+                         , touchscreenTestingMethod = testingMethodFromString p.touchscreenTestingMethod
+                         }
+         }, Cmd.none)
+
+{- Model -}
+
+type alias Model =
+    { preferences : Preferences
+    }
+
+type alias Preferences =
+    { preferencesSetup : Bool
+    , enableAnimations : Bool
+    , enableSounds : Bool
+    , enableVibration : Bool
+    , desktopTestingMethod : TestingMethod
+    , touchscreenTestingMethod : TestingMethod
+    }
+
+type TestingMethod
+    = FullWords
+    | FirstLetter
+    | OnScreen
+
+
+
+main : Program Flags Model Msg
 main =
-  beginnerProgram { model = 0, view = view, update = update }
+    programWithFlags
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
-view model = div [] [ text "Hello world! How are you?" ]
+view : Model -> Html.Html msg
+view model =
+    div []
+        [ div [] [ text "Hello world! How are you?" ]
+        , div [] [ text ("preferencesSetup: " ++ toString model.preferences.preferencesSetup) ]
+        , div [] [ text ("enableVibration: " ++ toString model.preferences.enableVibration) ]
+        , div [] [ text ("enableAnimations: " ++ toString model.preferences.enableAnimations) ]
+        , div [] [ text ("desktopTestingMethod: " ++ toString model.preferences.desktopTestingMethod) ]
+        ]
 
 
-type Msg = None
+subscriptions : a -> Sub msg
+subscriptions model =
+    Sub.none
 
 
+type Msg
+    = None
+
+
+update : Msg -> a -> ( a, Cmd msg )
 update msg model =
-  case msg of
-    None ->
-      model
+    case msg of
+        None ->
+            ( model, Cmd.none )
