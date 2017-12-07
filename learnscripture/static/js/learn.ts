58f5a2b9487dc44d8e1d6d2320d1730b3dc656d1
Luke Plant <L.Plant.98@cantab.net>
2017-12-07 20:53:40 +0300

    Removed clickTrapper from learn.ts
    This is legacy code for old Android versions we no longer support.

--- learnscripture/static/js/learn.ts
+++ learnscripture/static/js/learn.ts
@@ -30,7 +30,7 @@
 "use strict";
 
 // Imports
-import { isTouchDevice, ajaxRetrySucceeded, ajaxRetryOptions, ajaxRetryFailed, hideLoadingIndicator, indicateLoading, isAndroid, ajaxFailed } from './common';
+import { ajaxRetrySucceeded, ajaxRetryOptions, ajaxRetryFailed, hideLoadingIndicator, indicateLoading, ajaxFailed } from './common';
 import { doBeep, setUpAudio } from './sound';
 import { getPreferences } from './preferences'
 import { getAccountData } from './accounts';
@@ -1071,7 +1071,6 @@ $.extend(OnScreenTestingStrategy, {
         this.wordTestSetUp();
         this.ensureTestDivVisible();
         $('#id-hint-btn').hide();
-        this.clickTrapperSetUp();
     },
 
     testTearDown: function() {
@@ -1079,64 +1078,6 @@ $.extend(OnScreenTestingStrategy, {
         $('#id-onscreen-test-container').hide();
         this.removeTestDivFix();
         this.wordTestTearDown();
-        this.clickTrapperTearDown();
-    },
-
-    useClickTrapper: function() {
-        return (isAndroid() && isTouchDevice());
-    },
-
-    clickTrapperSetUp: function() {
-        if (!this.useClickTrapper()) {
-            return;
-        }
-        // Hack for Android bug with elements underneath receiving clicks
-        // https://code.google.com/p/android/issues/detail?id=6721#c16
-        // https://code.google.com/p/android/issues/detail?id=6721#c22
-        //
-        // This has a few moving parts:
-        //
-        // * event handler for clicks on body
-        //
-        // * event handler for touches on the absolutely positioned container
-        //
-        // The essential idea is that on touchstart for the absolutely
-        // positioned div, we set 'ignoreNextClick', and then ignore
-        // the next click that gets sent through to the body
-
-        var that = this;
-        this.clickTrapperTearDown();
-        this.ignoreNextClick = false;
-        var bodyClickTrapper = function(ev) {
-            if (that.ignoreNextClick) {
-                ev.preventDefault();
-                ev.stopPropagation();
-                that.ignoreNextClick = false;
-                return false;
-            } else {
-                return true;
-            }
-        };
-        $('body').bind('click', bodyClickTrapper);
-        this.bodyClickTrapper = bodyClickTrapper;
-
-        $('#id-onscreen-test-container').bind('touchstart',
-            function(ev) {
-                that.ignoreNextClick = true;
-            });
-
-    },
-
-    clickTrapperTearDown: function() {
-        if (!this.useClickTrapper()) {
-            return;
-        }
-        var bodyClickTrapper = this.bodyClickTrapper;
-        if (bodyClickTrapper !== undefined && bodyClickTrapper !== null) {
-            $('body').unbind('click', bodyClickTrapper);
-            this.bodyClickTrapper = null;
-        }
-        $('#id-onscreen-test-container').unbind('click');
     },
 
     wordTestSetUp: function() {
