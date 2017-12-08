c2c70b6694dcc52900a0416942dce7b052fdf057
Luke Plant <L.Plant.98@cantab.net>
2017-12-08 23:22:55 +0300

    Admin tweaks

--- awards/admin.py
+++ awards/admin.py
@@ -6,6 +6,8 @@ from .models import Award
 class AwardAdmin(admin.ModelAdmin):
 
     list_display = ['id', 'account', 'award_type', 'level', 'created']
+    list_filter = ['award_type', 'level']
+    search_fields = ['account__username']
 
     def get_queryset(self, request):
         return super(AwardAdmin, self).get_queryset(request).select_related('account')
--- events/admin.py
+++ events/admin.py
@@ -6,6 +6,7 @@ from .models import Event
 class EventAdmin(admin.ModelAdmin):
     list_display = ('id', 'account', 'event_type', 'weight', 'created')
     list_filter = ('event_type',)
+    raw_id_fields = ['parent_event', 'account_id']
 
     def get_queryset(self, *args, **kwargs):
         return super(EventAdmin, self).get_queryset(*args, **kwargs).select_related('account')
