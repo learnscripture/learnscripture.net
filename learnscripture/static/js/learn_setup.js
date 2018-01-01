5ca842f115ae60aa482a7291783b9474067e2ba7
Luke Plant <L.Plant.98@cantab.net>
2018-01-01 10:48:19 +0300

    New /learn/ page - attempt to fix on screen keyboard auto focus issues

--- learnscripture/static/js/learn_setup.js
+++ learnscripture/static/js/learn_setup.js
@@ -45,76 +45,124 @@ app.ports.updateTypingBox.subscribe(function (args) {
 
     // Also note that this subscribe function can get called before Elm has
     // updated the actual DOM, so handling this correctly is tricky.
-    var typingBoxId = args[0];
-    var wordButtonId = args[1];
-    var expectedClass = args[2];
-    var hardMode = args[3];
 
     var attempts = 0;
-    var fixTypingBox = function () {
+
+    var callFixTypingBox = function (delay,
+                                     typingBoxId, wordButtonId, expectedClass, hardMode) {
+        window.setTimeout(function () {
+            fixTypingBox(typingBoxId, wordButtonId, expectedClass, hardMode)
+        }, delay)
+    };
+
+    var fixTypingBox = function (typingBoxId, wordButtonId, expectedClass, hardMode) {
         if (attempts > 10) {
             return; // give up
         }
 
         var tryAgain = function () {
             attempts += 1;
-            callFixTypingBox(attempts * 10);
+            callFixTypingBox(attempts * 10, typingBoxId, wordButtonId, expectedClass, hardMode);
         }
 
         var typingBox = document.getElementById(typingBoxId);
         if (typingBox === null) {
-            tryAgain();
-            return;
+            if (expectedClass == "tohide") {
+                // Don't need to hide something that doesn't exist yet, and the
+                // typing box is hidden by default.
+                return;
+            } else {
+                tryAgain();
+                return;
+            }
         }
 
         var wordButton = wordButtonId == "" ? null : document.getElementById(wordButtonId);
 
-        if (wordButtonId != "" && wordButtonId == null) {
-            tryAgain();
-            return;
-        }
-
+        // Have we been called before the DOM was updated?
         var classes = typingBox.className.trim().split(/ /);
-        if (classes.indexOf(expectedClass) < 0) {
-            // We have been called before the DOM was updated.
-            tryAgain();
-            return;
-        }
+        var domUpdated = classes.indexOf(expectedClass) >= 0;
+
+        // We really need to wait for the DOM to be updated before
+        // we move/show/focus the typing box, especially when it first appears
+        // (otherwise, `wordButton` won't exist in the DOM, and we won't
+        // know where to move the typing box to).
+        //
+        // However, we want the `.focus()` call to cause onscreen keyboard to
+        // automatically appear. This only works if we do this operation
+        // directly within an event handler, not via a `setTimeout` call (at
+        // least with Firefox for Android).
+        //
+        // So, we have to show and focus the box even before the DOM is updated
+        // in some cases, and then run this routine again (using setTimeout) to
+        // move the typing box into the right place.
 
         if (expectedClass == "toshow") {
             // Fix size/position first, then make it visible.
-            var rect = wordButton.getClientRects()[0];
-            typingBox.style.top = rect.top.toString() + "px";
-            typingBox.style.left = rect.left.toString() + "px";
-            // Allow for border
-            var styles = window.getComputedStyle(typingBox);
-            var parsePx = function (p) {
-                return parseInt(p.replace("px", ""), 10);
-            }
-            var borderWidth = parsePx(styles['border-left-width']);
-            typingBox.style.height = (rect.height - 2 * borderWidth).toString() + "px";
 
-            if (hardMode) {
-                typingBox.style.width = "6em";
+            var correctPositioningComplete = false;
+            if (wordButton != null) {
+                var rect = wordButton.getClientRects()[0];
+                typingBox.style.top = rect.top.toString() + "px";
+                typingBox.style.left = rect.left.toString() + "px";
+                // Allow for border
+                var styles = window.getComputedStyle(typingBox);
+                var parsePx = function (p) {
+                    return parseInt(p.replace("px", ""), 10);
+                }
+                var borderWidth = parsePx(styles['border-left-width']);
+                typingBox.style.height = (rect.height - 2 * borderWidth).toString() + "px";
+
+                if (hardMode) {
+                    typingBox.style.width = "6em";
+                } else {
+                    // Allow for border and left padding
+                    var paddingLeft = parsePx(styles['padding-left']);
+                    var paddingRight = parsePx(styles['padding-right']);
+                    typingBox.style.width = (rect.width - 2 * borderWidth - paddingLeft - paddingRight).toString() + "px";
+                }
+                correctPositioningComplete = true;
             } else {
-                // Allow for border and left padding
-                var paddingLeft = parsePx(styles['padding-left']);
-                var paddingRight = parsePx(styles['padding-right']);
-                typingBox.style.width = (rect.width - 2 * borderWidth - paddingLeft - paddingRight).toString() + "px";
+                insertCss("body.learn-page #id-typing { " +
+                          "   top: 0px;" +
+                          "   left: 0px;" +
+                          "}");
             }
-            typingBox.style.display = "inline";
+            insertCss("body.learn-page #id-typing { " +
+                      "   display: inline-block;" +
+                      "}")
             typingBox.focus();
+
+            if (!correctPositioningComplete || !domUpdated) {
+                tryAgain();
+                return;
+            }
         } else if (expectedClass == "tohide") {
             typingBox.blur();
-            typingBox.style.display = "none";
+            insertCss("body.learn-page #id-typing { " +
+                      "   display: none;" +
+                      "}")
         }
     };
 
-    var callFixTypingBox = function (delay) {
-        window.setTimeout(fixTypingBox, delay)
-    }
+    // insertCss - changes need to be done perhaps via CSS rules to avoid
+    // modifying the DOM before Elm has. (TODO - really?)
+    var insertCss = function (code) {
+        var style = document.createElement('style');
+        style.type = 'text/css';
 
-    callFixTypingBox(1);
+        if (style.styleSheet) {
+            // IE
+            style.styleSheet.cssText = code;
+        } else {
+            // Other browsers
+            style.innerHTML = code;
+        }
+        document.getElementsByTagName("head")[0].appendChild(style);
+    };
+
+
+    fixTypingBox(args[0], args[1], args[2], args[3]);
 
 });
 
