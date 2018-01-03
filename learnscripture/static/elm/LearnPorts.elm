0e4d155ab4552882cc924a1d5559efbdf5a02374
Luke Plant <L.Plant.98@cantab.net>
2018-01-03 19:53:53 +0300

    Reformatting

--- learnscripture/static/elm/LearnPorts.elm
+++ learnscripture/static/elm/LearnPorts.elm
@@ -2,11 +2,17 @@ port module LearnPorts exposing (..)
 
 import Json.Decode as JD
 
-port updateTypingBox : { typingBoxId : String
-                       , wordButtonId : String
-                       , expectedClass : String
-                       , hardMode : Bool } -> Cmd msg
+
+port updateTypingBox :
+    { typingBoxId : String
+    , wordButtonId : String
+    , expectedClass : String
+    , hardMode : Bool
+    }
+    -> Cmd msg
+
 
 port receivePreferences : (JD.Value -> msg) -> Sub msg
+
 
 port vibrateDevice : Int -> Cmd msg
