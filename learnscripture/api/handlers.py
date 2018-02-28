7bc89a1ed7cf630ad7a78bf8be97ce6843f30cdf
Luke Plant <L.Plant.98@cantab.net>
2018-02-28 03:03:52 +0300

    Auto show help tour first time.

--- learnscripture/api/handlers.py
+++ learnscripture/api/handlers.py
@@ -685,5 +685,7 @@ class SaveMiscPreferences(ApiView):
             identity.pin_action_log_menu_small_screen = request.POST['pin_action_log_menu_small_screen'] == 'true'
         if 'pin_verse_options_menu_large_screen' in request.POST:
             identity.pin_verse_options_menu_large_screen = request.POST['pin_verse_options_menu_large_screen'] == 'true'
+        if 'seen_help_tour' in request.POST:
+            identity.seen_help_tour = request.POST['seen_help_tour'] == 'true'
         identity.save()
         return {}
