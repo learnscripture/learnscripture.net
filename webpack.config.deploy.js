1310cce65f545d4f9dba508ced15339ba1a60757
Luke Plant <L.Plant.98@cantab.net>
2018-02-28 09:14:42 +0300

    Got build process/uglifyjs to handle ES6 syntax

--- webpack.config.deploy.js
+++ webpack.config.deploy.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 var config = require('./webpack.config.base.js');
 
@@ -9,9 +10,7 @@ config.output.filename = "[name]-[hash].deploy.pack.js";
 config.plugins = config.plugins.concat([
     new BundleTracker({filename: './webpack-stats.deploy.json'}),
     new ExtractTextPlugin("[name]-[hash].deploy.css"),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true
-    })
+    new UglifyJsPlugin()
 ])
 
 module.exports = config;
