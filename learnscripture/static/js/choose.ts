2d7bf1601ebcc47ba730e48c5702b01488342328
Luke Plant <L.Plant.98@cantab.net>
2018-10-23 15:10:52 +0300

    Tabable controls on choose page, with nicer focus behaviour.

--- learnscripture/static/js/choose.ts
+++ learnscripture/static/js/choose.ts
@@ -10,7 +10,25 @@ var loadResults = function(results) {
 };
 
 var setupChooseControls = function() {
-    $('#id_lookup').click(quickFindAndHandleResults(loadResults, false, "choose-individual"));
+    $('#id-choose-verseset').bind('accordion:expanded', function(ev) {
+        // We want to not get in the way of mobile users who just want to browse
+        // the list, rather than search. So we use setTimeout here because
+        // on Firefox for Android at least, it means that the keyboard doesn't
+        // get raised.
+        window.setTimeout(function() {
+            $('#id_query').focus();
+        }, 0);
+    });
+
+    $('#id-choose-individual').bind('accordion:expanded', function(ev) {
+        $('#id_quick_find').focus();
+    });
+
+    $('#id-choose-create-set-menu').bind('accordion:expanded', function(ev) {
+        $(ev.target).find('.btn.primary')[0].focus();
+    });
+
+    $('#id_lookup').click(quickFindAndHandleResults(loadResults, false, "choose-individual"))
 
     $('#id-choose-individual').on('click', 'form.individual-choose input[name=add_to_queue]', function(ev) {
         ev.preventDefault();
