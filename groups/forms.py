cbf43b8581a4ca42bac414670b34fb18c975b147
Luke Plant <L.Plant.98@cantab.net>
2018-11-13 16:11:54 +0300

    i18n for groups

--- groups/forms.py
+++ groups/forms.py
@@ -3,18 +3,19 @@ from django import forms
 
 from accounts.models import Account
 from groups.models import Group
+from learnscripture.ftl_bundles import t, t_lazy
 
 
 class EditGroupForm(forms.ModelForm):
 
     invited_users = forms.ModelMultipleChoiceField(
-        label='Invited users',
+        label=t_lazy('groups-invited-users'),
         required=False,
         queryset=Account.objects.all(),
         widget=autocomplete.ModelSelect2Multiple(
             url='account_autocomplete',
             attrs={
-                'data-placeholder': 'Username...',
+                'data-placeholder': t_lazy('accounts-username'),
                 'data-minimum-input-length': 2,
             }),
     )
@@ -27,7 +28,7 @@ class EditGroupForm(forms.ModelForm):
         name = self.cleaned_data['name']
         name = name.strip()
         if name == '':
-            raise forms.ValidationError("This field is required")
+            raise forms.ValidationError(t('forms-field-required'))
         return name
 
 
@@ -35,5 +36,5 @@ f = EditGroupForm.base_fields['description'].widget.attrs
 del f['cols']
 f['rows'] = 3
 
-EditGroupForm.base_fields['public'].help_text = """A public group is visible to everyone, and so is the member list. This can't be undone once selected."""
-EditGroupForm.base_fields['open'].help_text = """Anyone can join an open group. For a closed group, you have to specifically invite people. A group must be public to be open."""
+EditGroupForm.base_fields['public'].help_text = t_lazy('groups-public-group-help-text')
+EditGroupForm.base_fields['open'].help_text = t_lazy('groups-open-group-help-text')
