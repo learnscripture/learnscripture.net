bad6816eca90e61524b9140a5597f796692230fe
Luke Plant <L.Plant.98@cantab.net>
2019-06-29 20:33:36 +0300

    cms - Nicer and more future-proof mechanism for selecting content items

--- cms/views.py
+++ cms/views.py
@@ -1,8 +1,40 @@
 from django.conf import settings
 from django.http import Http404, HttpResponsePermanentRedirect
 from django.template.response import TemplateResponse
+from django.utils.functional import cached_property
 
-from .models import Page
+from .models import Page as Page
+
+
+class PageWrapper:
+    def __init__(self, page):
+        self.page = page
+
+    @cached_property
+    def blocks(self):
+        return Blocks(self)
+
+    def title(self):
+        return self.page.title
+
+
+class Blocks:
+    """
+    Provides dictionary access to the content items in a block in a Page
+    """
+    def __init__(self, page_wrapper):
+        self.page_wrapper = page_wrapper
+
+    def __getitem__(self, block_name):
+        return [
+            pci.content_item
+            for pci in (
+                self.page_wrapper.page.page_content_items
+                    .filter(block_name=block_name)
+                    .order_by('sort')
+                    .select_related('content_item')
+            )
+        ]
 
 
 def cms_page(request):
@@ -21,6 +53,6 @@ def cms_page(request):
     if page.redirect_page and page.redirect_page != page:  # prevent redirecting to itself
         return HttpResponsePermanentRedirect(page.redirect_page.get_absolute_url())
     context = {
-        'cms_page': page
+        'page': PageWrapper(page)
     }
     return TemplateResponse(request, template_name, context)
