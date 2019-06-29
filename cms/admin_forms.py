0b10869c01e5f9696f5bdb5de7d52896801687d7
Luke Plant <L.Plant.98@cantab.net>
2019-06-29 16:21:20 +0300

    Removed unused cms meta_description and meta_keywords fields

--- cms/admin_forms.py
+++ cms/admin_forms.py
@@ -6,9 +6,6 @@ from cms.models import Page
 
 
 class PageForm(forms.ModelForm):
-
-    meta_description = forms.CharField(widget=forms.Textarea, required=False)
-    meta_keywords = forms.CharField(widget=forms.Textarea, required=False)
     parent = TreeNodeChoiceField(queryset=Page.tree.all(), level_indicator=3 * unichr(160), empty_label='---------', required=False)
     redirect_page = TreeNodeChoiceField(label='Redirect page', queryset=Page.objects.filter(redirect_page__isnull=True), level_indicator=3 * unichr(160), empty_label='---------', required=False)
 
