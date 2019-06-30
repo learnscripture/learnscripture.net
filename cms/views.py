85a94a7f18a86d96a3bf5828cc662ce8117e2ed8
Luke Plant <L.Plant.98@cantab.net>
2019-06-30 21:30:40 +0300

    isort fix

--- cms/views.py
+++ cms/views.py
@@ -4,7 +4,7 @@ from django.http import Http404, HttpResponsePermanentRedirect
 from django.template.response import TemplateResponse
 from django.utils.functional import cached_property
 
-from .models import Page, PageTitle, Content
+from .models import Content, Page, PageTitle
 
 
 class PageWrapper:
