e3e9ff832dce32cc295b43c8037653f035ed2e3f
Luke Plant <L.Plant.98@cantab.net>
2019-08-01 21:45:52 +0300

    Upgrades/fixes for Django 2.1

--- cms/utils/widgets.py
+++ cms/utils/widgets.py
@@ -8,16 +8,16 @@ from .images import DETAIL_THUMBNAIL_OPTIONS, ThumbnailException, get_thumbnail
 
 class CmsTextarea(forms.Textarea):
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         attrs['class'] = 'cms-editor'
-        return super(CmsTextarea, self).render(name, value, attrs)
+        return super(CmsTextarea, self).render(name, value, attrs=attrs, renderer=renderer)
 
 
 class AdminImageWidgetWithPreview(AdminFileWidget):
     """
     A Widget for an ImageField with a preview of the current image.
     """
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         output = []
         if value and isinstance(value, ImageFieldFile):
             file_name = str(value)
@@ -27,5 +27,5 @@ class AdminImageWidgetWithPreview(AdminFileWidget):
                     output.append(format_html('<img src="{0}" width="{1}" height="{2}" />', thumbnail.url, thumbnail.width, thumbnail.height))
             except ThumbnailException as e:
                 output.append(format_html('<p>{0}</p>', str(e)))
-        output.append(super(AdminImageWidgetWithPreview, self).render(name, value, attrs))
+        output.append(super(AdminImageWidgetWithPreview, self).render(name, value, attrs=attrs, renderer=renderer))
         return format_html_join('', '{0}', [(o,) for o in output])
