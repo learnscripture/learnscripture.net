d5934dc20d348717cb8d5823ed0dde7873bfd4c4
Luke Plant <L.Plant.98@cantab.net>
2019-08-11 19:19:21 +0300

    Fixed logging, upgraded to Sentry SDK

--- learnscripture/celery.py
+++ learnscripture/celery.py
@@ -1,41 +1,16 @@
 from __future__ import absolute_import
 
-import logging
 import os
 
 import django_ftl
 from celery import Celery
 from celery.app.task import Task
-from celery.signals import task_failure
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnscripture.settings')
 from django.conf import settings  # noqa isort:skip
 
 from . import checks_monkeypatch  # noqa isort:skip
-
-
-@task_failure.connect
-def log_exception(
-        task_id=None,
-        exception=None,
-        args=None,
-        kwargs=None,
-        traceback=None,
-        einfo=None,
-        **other):
-    from raven.contrib.django.raven_compat.handlers import SentryHandler
-    logger = logging.getLogger("celery task exception")
-    logger.addHandler(SentryHandler())
-    logger.error(
-        "celery task failure",
-        exc_info=einfo.exc_info,
-        extra={
-            'task_id': task_id,
-            'task_args': args,
-            'task_kwargs': kwargs,
-        }
-    )
 
 
 if settings.CELERY_TASK_ALWAYS_EAGER:
