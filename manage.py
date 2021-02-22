3f238f7a1801c520b13d478e8dc9c3a9f041e717
Luke Plant <L.Plant.98@cantab.net>
2021-02-22 10:02:10 +0300

    Stopped 'checks' from throwing exceptions (again)

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import sys
 import warnings
 
 warnings.simplefilter("once", PendingDeprecationWarning)
@@ -9,4 +10,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'learnscripture.settings_local'
 
 from django.core import management  # noqa isort:skip
 if __name__ == "__main__":
+    if any(val in sys.argv for val in ['runserver', 'check']):
+        import learnscripture.checks_monkeypatch  # noqa
     management.execute_from_command_line()
